11a030d6c74e53944ac504a6a78dba0924179bcd
Lingfan Gao <lingfangao@hotmail.com>
2023-05-19 12:17:42 +0200

    find tune dependencies

--- packages/nx-plugin/src/executors/build/executor.ts
+++ packages/nx-plugin/src/executors/build/executor.ts
@@ -60,11 +60,8 @@ function copyPackageJson(paths: PackagePaths) {
       ...packageJson.dependencies,
     },
     peerDependencies: {
-      '@fluentui/react-components': '>=9.0.0 <10.0.0',
       '@types/react': '>=16.8.0 <19.0.0',
-      '@types/react-dom': '>=16.8.0 <19.0.0',
       react: '>=16.8.0 <19.0.0',
-      'react-dom': '>=16.8.0 <19.0.0',
       ...packageJson.peerDependencies,
     },
     exports: {
