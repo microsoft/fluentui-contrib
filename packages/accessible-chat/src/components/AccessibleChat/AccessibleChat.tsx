df7138117e0de240d0a070f11b54e18f63be35c3
Adam Samec <adam.samec@gmail.com>
2023-06-19 19:10:25 +0200

    Add Control + Enter behavior on messageContorl

--- packages/accessible-chat/src/components/AccessibleChat/AccessibleChat.tsx
+++ packages/accessible-chat/src/components/AccessibleChat/AccessibleChat.tsx
@@ -5,7 +5,11 @@ import {
   Avatar,
   PresenceBadgeStatus,
 } from '@fluentui/react-components';
-import { Chat, ChatMessage, ChatMyMessage } from '@fluentui-contrib/react-chat';
+import {
+  Chat,
+  ChatMessage,
+  ChatMyMessage,
+ } from '@fluentui-contrib/react-chat';
 
 import { useStyles } from './AccessibleChat.styles';
 
@@ -18,6 +22,14 @@ export const AccessibleChat: React.FC = () => {
 
   const user1: User = {name: 'Ashley McCarthy', status: 'available'};
 
+  const handleMessageKeyDown = (id) => {
+return (event) => {
+  if (event.ctrlKey && event.key === 'Enter') {
+document.getElementById(id).focus();
+}
+};
+  };
+
   return (
   <div className={mergeClasses(styles.root)}>
 <button> start here</button>
@@ -26,8 +38,11 @@ export const AccessibleChat: React.FC = () => {
 <Chat>
       <ChatMessage avatar={<Avatar name={user1.name} badge={{ status: user1.status }} />}>Hello I am Ashley</ChatMessage>
       <ChatMyMessage>Nice to meet you!</ChatMyMessage>
-      <ChatMessage avatar={<Avatar name={user1.name} badge={{ status: user1.status }} />}>
-        <div role="none" tabIndex={0}>
+      <ChatMessage
+      avatar={<Avatar name={user1.name} badge={{ status: user1.status }} />}
+      onKeyDown={handleMessageKeyDown('message3-content')}
+      >
+        <div role="none" id="message3-content" tabIndex={0}>
         This is <a href="#">my homepage</a>. Some text goes here to demonstrate reading of longer runsof texts. Now follows <a href="#">another link</a> which is also a dummy link.
         </div>
       </ChatMessage>
