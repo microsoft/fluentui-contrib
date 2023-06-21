421c4e99e9785d6a8f592218f6e028a5df0c71ac
Adam <adam.samec@gmail.com>
2023-06-21 10:50:53 +0200

    Add chat with focusable content for testing with JAWS (#11)
    Co-authored-by: Lingfan Gao <lingfan.gao@microsoft.com>

---
+++ packages/react-chat/stories/Chat/ChatWithFocusableContent.stories.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import {
+  Avatar,
+  useFluent,
+  PresenceBadgeStatus,
+} from '@fluentui/react-components';
+import { Chat, ChatMessage, ChatMyMessage } from '@fluentui-contrib/react-chat';
+
+interface User {
+  name: string;
+  status: PresenceBadgeStatus;
+}
+
+interface CustomChatMessageProps {
+  user?: User;
+  contentId: string;
+  children: React.ReactNode;
+}
+
+const ChatMessageContent: React.FC<React.HTMLAttributes<HTMLDivElement>> = (
+  props
+) => <div {...props} aria-readingmode="true" />;
+
+const CustomChatMessage: React.FC<CustomChatMessageProps> = ({
+  user,
+  contentId,
+  children,
+}) => {
+  const { targetDocument } = useFluent();
+  const handleMessageKeyDown = (event: React.KeyboardEvent) => {
+    if (event.ctrlKey && event.key === 'Enter') {
+      targetDocument?.getElementById(contentId)?.focus();
+    }
+  };
+
+  return (
+    <>
+      {user ? (
+        <ChatMessage
+          avatar={<Avatar name={user.name} badge={{ status: user.status }} />}
+          onKeyDown={handleMessageKeyDown}
+        >
+          <ChatMessageContent id={contentId}>{children}</ChatMessageContent>
+        </ChatMessage>
+      ) : (
+        <ChatMyMessage onKeyDown={handleMessageKeyDown}>
+          <ChatMessageContent id={contentId}>{children}</ChatMessageContent>
+        </ChatMyMessage>
+      )}
+    </>
+  );
+};
+
+export const ChatWithFocusableContent: React.FC = () => {
+  const user1: User = { name: 'Ashley McCarthy', status: 'available' };
+
+  return (
+    <div>
+      <h1>Chat with focusable content</h1>
+      <button> start here</button>
+
+      <Chat>
+        <CustomChatMessage user={user1} contentId="message1-content">
+          Hello I am Ashley
+        </CustomChatMessage>
+        <CustomChatMessage contentId="message2-content">
+          Nice to meet you!
+        </CustomChatMessage>
+        <CustomChatMessage user={user1} contentId="message3-content">
+          This is <a href="#">my homepage</a>. Some text goes here to
+          demonstrate reading of longer runs of texts. Now follows{' '}
+          <a href="#">another link</a> which is also a dummy link.
+        </CustomChatMessage>
+      </Chat>
+    </div>
+  );
+};
--- packages/react-chat/stories/Chat/index.stories.tsx
+++ packages/react-chat/stories/Chat/index.stories.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Meta } from '@storybook/react';
 import { Chat } from '@fluentui-contrib/react-chat';
 export { Default } from './Default.stories';
+export { ChatWithFocusableContent } from './ChatWithFocusableContent.stories';
 
 const meta: Meta<typeof Chat> = {
   component: Chat,
