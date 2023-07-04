5a85a5fdff430995dcf957642bc88e3c4b5f0674
Lingfan Gao <lingfan.gao@microsoft.com>
2023-07-04 13:36:15 +0000

    copy unavailable utilities

---
+++ packages/react-chat/src/components/utils/mergeCallbacks.ts
@@ -0,0 +1,13 @@
+/**
+ * TODO - replace this with Fluent UI utility
+ * https://github.com/microsoft/fluentui/pull/28428
+ */
+export function mergeCallbacks<Args extends unknown[]>(
+  callback1: ((...args: Args) => void) | undefined,
+  callback2: ((...args: Args) => void) | undefined
+) {
+  return (...args: Args) => {
+    callback1?.(...args);
+    callback2?.(...args);
+  };
+}
---
+++ packages/react-chat/src/components/utils/useEventCallback.ts
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { useIsomorphicLayoutEffect } from '@fluentui/react-components';
+
+/**
+ * TODO - replace this with Fluent UI utility
+ * https://github.com/microsoft/fluentui/pull/28428
+ */
+export const useEventCallback = <Args extends unknown[], Return>(
+  fn: (...args: Args) => Return
+) => {
+  const callbackRef = React.useRef<typeof fn>(() => {
+    throw new Error('Cannot call an event handler while rendering');
+  });
+
+  useIsomorphicLayoutEffect(() => {
+    callbackRef.current = fn;
+  }, [fn]);
+
+  return React.useCallback(
+    (...args: Args) => {
+      const callback = callbackRef.current;
+      return callback(...args);
+    },
+    [callbackRef]
+  );
+};
