6e49b90efa2e3f808a3a59c5862967fc0e3abb7d
Lingfan Gao <lingfan.gao@microsoft.com>
2023-07-05 19:34:17 +0000

    fix formatting

--- packages/react-shadow/.storybook/main.ts
+++ packages/react-shadow/.storybook/main.ts
@@ -24,4 +24,3 @@ export default config;
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/packages/storybook/documents/custom-builder-configs
-
