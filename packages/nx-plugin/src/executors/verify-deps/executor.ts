cf3d3a3de981b66dffc460731f497a3b06837fb5
Lingfan Gao <lingfan.gao@microsoft.com>
2023-07-05 14:50:33 +0000

    chore: Add verify-deps executor to verify peer deps
    Adds a new executor `verify` deps that will copy individual packages to
    a tmp directory to build them with typescript with the minimum peer dependency versions.
    If the build fails, it should be an indicator that peer dependencies are
    not correct.

---
+++ packages/nx-plugin/src/executors/verify-deps/executor.ts
@@ -0,0 +1,104 @@
+import { ExecutorContext, readJsonFile, writeJsonFile } from '@nx/devkit';
+import * as tmp from 'tmp';
+import { copySync } from 'fs-extra';
+import * as path from 'path';
+import * as semver from 'semver';
+import { execSync } from 'child_process';
+import { VerifyDepsExecutorSchema } from './schema';
+import { PackagePaths, getPackagePaths } from '../../utils';
+
+export default async function runExecutor(
+  options: VerifyDepsExecutorSchema,
+  context: ExecutorContext
+) {
+  const { cleanup: shouldCleanup } = options;
+  const { projectName, projectsConfigurations, root: workspaceRoot } = context;
+  if (!projectsConfigurations || !projectName) {
+    throw new Error('no project configurations');
+  }
+
+  const { root } = projectsConfigurations.projects[projectName];
+  const { name: tmpDir, removeCallback: cleanup } = tmp.dirSync({
+    prefix: projectName,
+    unsafeCleanup: true,
+  });
+  console.info('copying', projectName, 'to', tmpDir);
+  copySync(root, tmpDir);
+  const paths = getPackagePaths(tmpDir, '.', true);
+
+  configureTsConfig(paths, workspaceRoot);
+
+  configurePackageJson(paths, workspaceRoot);
+
+  let success = true;
+  try {
+    const yarnCmd = 'npx yarn@1 --silent';
+    console.log('Run', yarnCmd);
+    execSync(yarnCmd, { stdio: 'inherit', cwd: paths.root });
+
+    const tscCmd =
+      'npx -p typescript tsc --noEmit --project ./tsconfig.lib.json';
+    console.log('Run', tscCmd);
+    execSync(tscCmd, {
+      stdio: 'inherit',
+      cwd: paths.root,
+    });
+  } catch {
+    success = false;
+    console.error(
+      '======================================================================'
+    );
+    console.error(
+      'verify-deps check failed. Most likely the minimum version of your peer dependency ranges need to be updated!'
+    );
+    console.error('You can debug by opening the test project', tmpDir);
+    console.error(
+      'and run the command `npx -p typescript tsc --noEmit --project ./tsconfig.lib.json`'
+    );
+  }
+
+  if (shouldCleanup && success) {
+    cleanup();
+  }
+  return {
+    success,
+  };
+}
+
+function configureTsConfig(paths: PackagePaths, workspaceRoot: string) {
+  copySync(
+    path.join(workspaceRoot, 'tsconfig.base.json'),
+    path.join(paths.root, 'tsconfig.base.json')
+  );
+
+  const tsconfig = readJsonFile(paths.tsconfig);
+  tsconfig.extends = './tsconfig.base.json';
+  writeJsonFile(paths.tsconfig, tsconfig);
+}
+
+function configurePackageJson(paths: PackagePaths, workspaceRoot: string) {
+  const pkgJson = readJsonFile(paths.packageJson);
+  pkgJson.dependencies ??= {};
+  const peerDeps = Object.entries(pkgJson.peerDependencies) as Array<
+    [string, string]
+  >;
+  // use minimum version of peer deps to test
+  for (const [depName, versionRange] of peerDeps) {
+    const minVersion = semver.minVersion(versionRange);
+    if (minVersion) {
+      pkgJson.dependencies[depName] = minVersion.version;
+    }
+  }
+
+  // Copy all types dependencies - they are not that big and there's not many of them (yet)
+  const rootPkgJson = readJsonFile(path.join(workspaceRoot, 'package.json'));
+  const rootDevDeps = Object.entries(rootPkgJson.devDependencies ?? {});
+  pkgJson.devDependencies ??= {};
+  for (const [depName, version] of rootDevDeps) {
+    if (depName.startsWith('@types')) {
+      pkgJson.devDependencies[depName] = version;
+    }
+  }
+
+  writeJsonFile(paths.packageJson, pkgJson);
+}
--- packages/nx-plugin/src/generators/library/generator.spec.ts
+++ packages/nx-plugin/src/generators/library/generator.spec.ts
@@ -32,10 +32,7 @@ describe('create-package generator', () => {
     expect(pkgJson.peerDependencies).toMatchInlineSnapshot(`
       {
         "@fluentui/react-components": ">=9.25.1 <10.0.0",
-        "@types/react": ">=16.8.0 <19.0.0",
-        "@types/react-dom": ">=16.8.0 <19.0.0",
         "react": ">=16.8.0 <19.0.0",
-        "react-dom": ">=16.8.0 <19.0.0",
       }
     `);
   });
@@ -56,6 +53,16 @@ describe('create-package generator', () => {
     expect(config.targets?.['type-check']).toMatchInlineSnapshot(`
       {
         "executor": "@fluentui-contrib/nx-plugin:type-check",
+      }
+    `);
+  });
+
+  it('should verify-deps target', async () => {
+    await generator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config.targets?.['verify-deps']).toMatchInlineSnapshot(`
+      {
+        "executor": "@fluentui-contrib/nx-plugin:verify-deps",
       }
     `);
   });
