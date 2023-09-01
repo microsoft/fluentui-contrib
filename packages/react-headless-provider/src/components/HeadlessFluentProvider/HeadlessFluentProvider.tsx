03f7f464b743a5717d85e34f5d2ae5c449c28435
Oleksandr Fediashov <olfedias@microsoft.com>
2023-09-01 12:41:23 +0200

    feat: add HeadlessFluentProvider to @fluentui-contrib/react-headless-provider

--- packages/react-headless-provider/src/components/HeadlessFluentProvider/HeadlessFluentProvider.tsx
+++ packages/react-headless-provider/src/components/HeadlessFluentProvider/HeadlessFluentProvider.tsx
@@ -1,8 +1,90 @@
 import * as React from 'react';
-import { mergeClasses } from '@fluentui/react-components';
-import { useStyles } from './HeadlessFluentProvider.styles';
+import {
+  useFluent,
+  renderFluentProvider_unstable,
+  useFluentProviderContextValues_unstable,
+} from '@fluentui/react-components';
+import type {
+  FluentProviderProps,
+  FluentProviderState,
+} from '@fluentui/react-components';
+import {
+  CustomStyleHooksContext_unstable as CustomStyleHooksContext,
+  useOverrides_unstable as useOverrides,
+} from '@fluentui/react-shared-contexts';
+import type { CustomStyleHooksContextValue_unstable as CustomStyleHooksContextValue } from '@fluentui/react-shared-contexts';
 
-export const HeadlessFluentProvider: React.FC = () => {
-  const styles = useStyles();
-  return <div className={mergeClasses(styles.root)}>Hello World!</div>;
+type HeadlessFluentProviderProps = Omit<
+  FluentProviderProps,
+  'applyStylesToPortals' | 'theme'
+>;
+
+function shallowMerge<T>(a: T, b: T): T {
+  // Merge impacts perf: we should like to avoid it if it's possible
+  if (a && b) {
+    return { ...a, ...b };
+  }
+
+  if (a) {
+    return a;
+  }
+
+  return b;
+}
+
+function useHeadlessFluentProviderState(
+  props: HeadlessFluentProviderProps
+): FluentProviderState {
+  const parentContext = useFluent();
+  const parentOverrides = useOverrides();
+  const parentCustomStyleHooks: CustomStyleHooksContextValue =
+    React.useContext(CustomStyleHooksContext) || {};
+
+  const {
+    customStyleHooks_unstable,
+    children,
+    dir = parentContext.dir,
+    targetDocument = parentContext.targetDocument,
+    overrides_unstable: overrides = {},
+  } = props;
+
+  const mergedOverrides = shallowMerge(parentOverrides, overrides);
+  const mergedCustomStyleHooks = shallowMerge(
+    parentCustomStyleHooks,
+    customStyleHooks_unstable
+  ) as CustomStyleHooksContextValue;
+
+  return {
+    // "dir" & "document" needs to be specified for proper rendering of children
+    dir,
+    targetDocument,
+
+    customStyleHooks_unstable: mergedCustomStyleHooks,
+    overrides_unstable: mergedOverrides,
+
+    // In headless mode, we don't render any elements
+
+    components: { root: React.Fragment },
+    root: { children },
+
+    // Styling is ignored in headless mode
+
+    themeClassName: '',
+    theme: {},
+
+    applyStylesToPortals: false,
+    serverStyleProps: {
+      cssRule: '',
+      attributes: {},
+    },
+  };
+}
+
+export const HeadlessFluentProvider: React.FC<HeadlessFluentProviderProps> = (
+  props
+) => {
+  const state = useHeadlessFluentProviderState(props);
+  const contextValues = useFluentProviderContextValues_unstable(state);
+
+  return renderFluentProvider_unstable(state, contextValues);
 };
--- packages/react-headless-provider/stories/HeadlessFluentProvider/Default.stories.tsx
+++ packages/react-headless-provider/stories/HeadlessFluentProvider/Default.stories.tsx
@@ -1,4 +1,103 @@
-import * as React from 'react';
+import {
+  createDOMRenderer,
+  makeStyles,
+  Button,
+  RendererProvider,
+  Tooltip,
+  shorthands,
+  webLightTheme,
+} from '@fluentui/react-components';
+import type { PartialTheme } from '@fluentui/react-components';
 import { HeadlessFluentProvider } from '@fluentui-contrib/react-headless-provider';
+import * as React from 'react';
+import Frame, { useFrame } from 'react-frame-component';
 
-export const Default = () => <HeadlessFluentProvider />;
+const useClasses = makeStyles({
+  container: {
+    display: 'flex',
+    ...shorthands.gap('5px'),
+  },
+});
+
+// TODO: should come from Fluent
+function createCSSRuleFromTheme(
+  theme: PartialTheme | undefined,
+  selector: string
+): string {
+  if (theme) {
+    const cssVarsAsString = (
+      Object.keys(theme) as (keyof typeof theme)[]
+    ).reduce((cssVarRule, cssVar) => {
+      return `${cssVarRule}--${cssVar}: ${theme[cssVar]}; `;
+    }, '');
+
+    return `${selector} { ${cssVarsAsString} }`;
+  }
+
+  return '';
+}
+
+const Container: React.FC = () => {
+  const classes = useClasses();
+
+  return (
+    <div className={classes.container}>
+      <Tooltip content="Hello, world!" relationship="label">
+        <Button>Hover me</Button>
+      </Tooltip>
+
+      <Button appearance="primary">Primary</Button>
+      <Button appearance="subtle">Subtle</Button>
+    </div>
+  );
+};
+
+const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { document: targetDocument } = useFrame();
+  const renderer = React.useMemo(
+    () => createDOMRenderer(targetDocument),
+    [targetDocument]
+  );
+
+  return (
+    <RendererProvider renderer={renderer} targetDocument={targetDocument}>
+      <HeadlessFluentProvider targetDocument={targetDocument}>
+        {children}
+      </HeadlessFluentProvider>
+    </RendererProvider>
+  );
+};
+
+const ThemeInjector: React.FC<{ theme: PartialTheme }> = ({ theme }) => {
+  const cssRule = React.useMemo(
+    () => createCSSRuleFromTheme(theme, ':root'),
+    [theme]
+  );
+
+  return <style dangerouslySetInnerHTML={{ __html: cssRule }} />;
+};
+
+export const Default = () => {
+  return (
+    <>
+      <p>
+        This example uses <code>iframe</code> to sandbox styles, but it's not
+        mandatory in a real world.
+      </p>
+
+      <p>
+        This example uses <code>HeadlessFluentProvider</code> to provide proper
+        context values for components (including a proper <code>document</code>{' '}
+        instance), but does not apply CSS for the theme. CSS for theme is
+        applied globally (to `:root`) in this example.
+      </p>
+
+      <Frame style={{ border: '2px dotted green', width: 500 }}>
+        <ThemeInjector theme={webLightTheme} />
+        <AppProvider>
+          <Container />
+        </AppProvider>
+      </Frame>
+    </>
+  );
+};
