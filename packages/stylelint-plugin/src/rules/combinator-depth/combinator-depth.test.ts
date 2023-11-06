88327b4ea52f372aaa62c9493557be94037359d5
ling1726 <lingfangao@hotmail.com>
2023-11-06 16:33:26 +0100

    feat: :hover and :active pseudoclasses should not count to combinator depth (#50)

--- packages/stylelint-plugin/src/rules/combinator-depth/combinator-depth.test.ts
+++ packages/stylelint-plugin/src/rules/combinator-depth/combinator-depth.test.ts
@@ -74,6 +74,43 @@ describe('combinator-depth', () => {
     );
   });
 
+  it('should not report error if the second token is a pseudo class', async () => {
+    const { errored, results } = await stylelint.lint({
+      code: `.foo:hover div { color: red; }`,
+      config: {
+        pluginFunctions: {
+          [rule.ruleName]: rule,
+        },
+        rules: {
+          [rule.ruleName]: [0],
+        },
+      },
+    });
+
+    expect(errored).toBe(false);
+    expect(results.length).toBe(1);
+    expect(results[0].warnings.length).toBe(0);
+  });
+
+  it('should report error if combinator depth after a pseudo class exceeds allowed depth', async () => {
+    const { errored, results } = await stylelint.lint({
+      code: `.foo:hover div div { color: red; }`,
+      config: {
+        pluginFunctions: {
+          [rule.ruleName]: rule,
+        },
+        rules: {
+          [rule.ruleName]: [0],
+        },
+      },
+    });
+
+    expect(errored).toBe(true);
+    expect(results.length).toBe(1);
+    expect(results[0].warnings.length).toBe(1);
+    expect(results[0].warnings[0].text).toContain(`maximum allowed depth of 0`);
+  });
+
   it.each([false, 'a', '%'])(
     'should throw an error if not configured with an %s',
     (allowedDepth) => {
