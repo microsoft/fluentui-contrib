0a08c029200613fc8097d2d156ee645930b1aa2f
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2024-01-12 15:28:56 +0100

    how did this get removed

---
+++ packages/react-chat/stories/Chat/index.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from '@storybook/react';
+import { Chat } from '@fluentui-contrib/react-chat';
+export { Default } from './Default.stories';
+export { ChatWithFocusableContent } from './ChatWithFocusableContent.stories';
+
+const meta: Meta<typeof Chat> = {
+  component: Chat,
+};
+
+export default meta;
