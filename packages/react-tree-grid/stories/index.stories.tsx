471bc7cf9c4d5402736afcabc1e3c285ef3f197d
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2024-01-17 13:02:05 +0000

    chore(react-tree): adds Adam's variant C example as reference

--- packages/react-tree-grid/stories/index.stories.tsx
+++ packages/react-tree-grid/stories/index.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta } from '@storybook/react';
 import { TreeGrid } from '@fluentui-contrib/react-tree-grid';
 
-export { VariantB } from './TreeGridVariantB.stories';
+export { AdamsTreeGridActiveOnlyEntireRowNarration } from './AdamsTreeGridActiveOnlyEntireRowNarration.stories';
 
 const meta: Meta<typeof TreeGrid> = {
   title: 'TreeGrid',
