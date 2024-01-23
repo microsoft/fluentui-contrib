bd9593f0f9f8201b3d9380904a8619d258dfc158
Martin Hochel <hochelmartin@gmail.com>
2024-01-23 17:15:15 +0100

    test(nx-plugin): make sure configure-storybook works

--- packages/nx-plugin/src/generators/configure-storybook/generator.spec.ts
+++ packages/nx-plugin/src/generators/configure-storybook/generator.spec.ts
@@ -1,20 +1,39 @@
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nx/devkit';
+import { Tree, joinPathFragments, readProjectConfiguration } from '@nx/devkit';
+
+import { default as libraryGenerator } from '../library/generator';
+import { createCodeowners } from '../../utils-testing';
 
 import generator from './generator';
 import { ConfigureStorybookGeneratorSchema } from './schema';
 
 describe('configure-storybook generator', () => {
   let tree: Tree;
-  const options: ConfigureStorybookGeneratorSchema = { name: 'test' };
+  const options: ConfigureStorybookGeneratorSchema = { name: 'hello' };
 
-  beforeEach(() => {
+  beforeEach(async () => {
     tree = createTreeWithEmptyWorkspace();
+    createCodeowners(tree);
+
+    await libraryGenerator(tree, { name: 'hello', owner: '@MrWick' });
   });
 
-  xit('should run successfully', async () => {
+  it('should generate storybook config including stories/ folder', async () => {
     await generator(tree, options);
-    const config = readProjectConfiguration(tree, 'test');
-    expect(config).toBeDefined();
+    const config = readProjectConfiguration(tree, 'hello');
+
+    expect(tree.children(joinPathFragments(config.root, '.storybook')))
+      .toMatchInlineSnapshot(`
+      [
+        "main.ts",
+        "preview.ts",
+        "tsconfig.json",
+        "preview.tsx",
+      ]
+    `);
+
+    expect(
+      tree.exists(joinPathFragments(config.root, 'stories/.gitkeep'))
+    ).toBe(true);
   });
 });
