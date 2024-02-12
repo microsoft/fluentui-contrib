a85e11ed74537e9f8577dcda040ea42f9dd8a154
Martin Hochel <hochelmartin@gmail.com>
2024-02-12 17:56:08 +0100

    fix(nx-plugin): port all schema options from nx/playwright and make our executor work

--- packages/nx-plugin/src/executors/playwright/executor.ts
+++ packages/nx-plugin/src/executors/playwright/executor.ts
@@ -1,7 +1,14 @@
-import { execSync } from 'child_process';
-import { ExecutorContext } from '@nx/devkit';
-import { PlaywrightExecutorSchema } from './schema';
+import { execSync, fork } from 'node:child_process';
+import {
+  type ExecutorContext,
+  getPackageManagerCommand,
+  names,
+  output,
+  workspaceRoot,
+} from '@nx/devkit';
 import { playwrightExecutor } from '@nx/playwright';
+
+import { type PlaywrightExecutorSchema } from './schema';
 
 export default async function runExecutor(
   options: PlaywrightExecutorSchema,
@@ -11,19 +18,112 @@ export default async function runExecutor(
     return playwrightExecutor(options, context);
   }
 
+  return playwrightCTExecutor(options, context);
+}
+
+/**
+ *
+ * Most of this code is copied from https://github.com/nrwl/nx/blob/master/packages/playwright/src/executors/playwright/playwright.impl.ts
+ */
+function playwrightCTExecutor(
+  options: PlaywrightExecutorSchema,
+  context: ExecutorContext
+) {
   if (!context.projectsConfigurations || !context.projectName) {
     throw new Error('no project configurations');
   }
 
-  const { root } = context.projectsConfigurations.projects[context.projectName];
+  const projectRoot =
+    context.projectsConfigurations.projects[context.projectName].root;
 
-  if (!root) {
-    throw new Error('No root root found in projects configurations');
+  if (!projectRoot) {
+    throw new Error(
+      `Unable to find the Project Root for ${context.projectName}. Is it set in the project.json?`
+    );
   }
 
-  execSync(`npx playwright test`, { cwd: root });
+  if (!options.skipInstall) {
+    output.log({
+      title: 'Ensuring Playwright is installed.',
+      bodyLines: ['use --skipInstall to skip installation.'],
+    });
+    const pmc = getPackageManagerCommand();
+    execSync(`${pmc.exec} playwright install`, {
+      cwd: workspaceRoot,
+      stdio: 'inherit',
+    });
+  }
 
-  return {
-    success: true,
-  };
+  const args = createArgs(options);
+
+  const childProcess = runPlaywright(args, context.root);
+  childProcess.stdout?.on('data', (message) => {
+    process.stdout.write(message);
+  });
+  childProcess.stderr?.on('data', (message) => {
+    process.stderr.write(message);
+  });
+
+  return new Promise<{ success: boolean }>((resolve) => {
+    childProcess.on('close', (code) => {
+      resolve({ success: code === 0 });
+    });
+  });
+}
+
+/**
+ *
+ * Most of this code is copied from https://github.com/nrwl/nx/blob/master/packages/playwright/src/executors/playwright/playwright.impl.ts
+ */
+function createArgs(
+  opts: PlaywrightExecutorSchema,
+  exclude: string[] = ['skipInstall', 'testingType']
+): string[] {
+  const args: string[] = [];
+
+  const { testFiles, ...rest } = opts;
+  if (testFiles) {
+    args.push(...testFiles);
+  }
+
+  for (const key in rest) {
+    if (exclude.includes(key)) continue;
+
+    const value = opts[key as keyof typeof rest];
+    // NOTE: playwright doesn't accept pascalCase args, only kebab-case
+    const arg = names(key).fileName;
+
+    if (Array.isArray(value)) {
+      args.push(...value.map((v) => `--${arg}=${v.trim()}`));
+    } else if (typeof value === 'boolean') {
+      // NOTE: playwright don't accept --arg=false, instead just don't pass the arg.
+      if (value) {
+        args.push(`--${arg}`);
+      }
+    } else {
+      args.push(`--${arg}=${value}`);
+    }
+  }
+
+  return args;
+}
+
+/**
+ *
+ * Most of this code is copied from https://github.com/nrwl/nx/blob/master/packages/playwright/src/executors/playwright/playwright.impl.ts
+ */
+function runPlaywright(args: string[], cwd: string) {
+  try {
+    const cli = require.resolve('@playwright/experimental-ct-core/cli');
+
+    return fork(cli, ['test', ...args], {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+      cwd,
+    });
+  } catch (e) {
+    console.error(e);
+    throw new Error(
+      'Unable to run playwright component tests. Is @playwright/experimental-ct-core installed?'
+    );
+  }
 }
--- packages/nx-plugin/src/executors/playwright/schema.d.ts
+++ packages/nx-plugin/src/executors/playwright/schema.d.ts
@@ -5,4 +5,55 @@ export interface PlaywrightExecutorSchema extends NxPlaywrightExecutorSchema {
    * @default 'component'
    */
   testingType: 'component' | 'e2e';
+
+  // ============ BELOW API COPIED FROM https://github.com/nrwl/nx/blob/master/packages/playwright/src/executors/playwright/playwright.impl.ts ============
+
+  /*
+   * if 'projects' is configured then that name needs to be provided instead of
+   * all, chromium, firefox, webkit
+   **/
+  browser?: 'all' | 'chromium' | 'firefox' | 'webkit' | string;
+  config?: string;
+  debug?: boolean;
+  forbidOnly?: boolean;
+  fullyParallel?: boolean;
+  grep?: string;
+  globalTimeout?: number;
+  grepInvert?: string;
+  testFiles?: string[];
+  headed?: boolean;
+  ignoreSnapshots?: boolean;
+  workers?: string;
+  list?: boolean;
+  maxFailures?: number | boolean;
+  noDeps?: boolean;
+  output?: string;
+  passWithNoTests?: boolean;
+  project?: string[];
+  quiet?: boolean;
+  repeatEach?: number;
+  reporter?:
+    | 'list'
+    | 'line'
+    | 'dot'
+    | 'json'
+    | 'junit'
+    | 'null'
+    | 'github'
+    | 'html'
+    | 'blob';
+  retries?: number;
+  shard?: string;
+  timeout?: number;
+  trace?:
+    | 'on'
+    | 'off'
+    | 'on-first-retry'
+    | 'on-all-retries'
+    | 'retain-on-failure';
+  updateSnapshots?: boolean;
+  ui?: boolean;
+  uiHost?: string;
+  uiPort?: string;
+  skipInstall?: boolean;
 }
