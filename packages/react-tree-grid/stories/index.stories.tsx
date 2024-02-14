cd585fb964caaafd73a8b0781faec41dda4988f9
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2024-02-14 13:10:08 +0100

    feature(react-tree-grid): TreeGridInteraction component (#97)

--- packages/react-tree-grid/stories/index.stories.tsx
+++ packages/react-tree-grid/stories/index.stories.tsx
@@ -4,6 +4,7 @@ import { TreeGrid } from '@fluentui-contrib/react-tree-grid';
 export { Default } from './Default.stories';
 export { Meetings } from './Meetings.stories';
 export { Virtualization } from './Virtualization.stories';
+export { Interaction } from './Interaction.stories';
 
 const meta: Meta<typeof TreeGrid> = {
   title: 'TreeGrid',
