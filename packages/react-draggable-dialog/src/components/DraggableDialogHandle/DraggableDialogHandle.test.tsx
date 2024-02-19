f7e54d64f298ad48eeb62e41b408368c36cc4442
Marcos Moura <marcosvmmoura@gmail.com>
2024-02-19 22:14:30 +0100

    test: add unit test to cover base state scenarios

--- packages/react-draggable-dialog/src/components/DraggableDialogHandle/DraggableDialogHandle.test.tsx
+++ packages/react-draggable-dialog/src/components/DraggableDialogHandle/DraggableDialogHandle.test.tsx
@@ -1,18 +1,44 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
 import { DraggableDialogHandle } from './DraggableDialogHandle';
+import { useDraggableDialogHandle } from './useDraggableDialogHandle';
+
+import * as contexts from '../../contexts/DraggableDialogContext';
+import * as dnd from '@dnd-kit/core';
+import { DraggableDialogHandleState } from './DraggableDialogHandle.types';
+
+jest.mock('../../contexts/DraggableDialogContext', () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual('../../contexts/DraggableDialogContext'),
+  };
+});
+
+jest.mock('@dnd-kit/core', () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual('@dnd-kit/core'),
+  };
+});
 
 describe('DraggableDialogHandle', () => {
-  let consoleErrorMock: jest.SpyInstance;
+  let consoleErrorSpy: jest.SpyInstance;
+  let useDraggableDialogContextSpy: jest.SpyInstance;
+  let useDraggableSpy: jest.SpyInstance;
 
   beforeEach(() => {
-    consoleErrorMock = jest
+    consoleErrorSpy = jest
       .spyOn(console, 'error')
       .mockImplementation((error: string) => error);
+    useDraggableDialogContextSpy = jest.spyOn(
+      contexts,
+      'useDraggableDialogContext'
+    );
+    useDraggableSpy = jest.spyOn(dnd, 'useDraggable');
   });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   it('should render', () => {
@@ -26,6 +52,59 @@ describe('DraggableDialogHandle', () => {
     expect(() => getByText(text)).toBeTruthy();
   });
 
+  it('should not wrap children in any additional elements', () => {
+    const { container, rerender } = render(
+      <DraggableDialogHandle>
+        <div className="custom-handle">Handle</div>
+      </DraggableDialogHandle>
+    );
+    const getHandle = () => container.firstChild as HTMLElement;
+    let handle = getHandle();
+
+    expect(handle).toBeTruthy();
+    expect(handle.tagName.toLowerCase()).toBe('div');
+    expect(handle.classList.contains('custom-handle')).toBe(true);
+    expect(handle.classList.contains('fui-DraggableDialogHandle')).toBe(true);
+
+    rerender(
+      <DraggableDialogHandle>
+        <button className="button">Handle</button>
+      </DraggableDialogHandle>
+    );
+
+    handle = getHandle();
+
+    expect(handle.tagName.toLowerCase()).toBe('button');
+    expect(handle.classList.contains('button')).toBe(true);
+    expect(handle.classList.contains('custom-handle')).toBe(false);
+    expect(handle.classList.contains('fui-DraggableDialogHandle')).toBe(true);
+  });
+
+  it('should add draggable props to the child', () => {
+    const mockAttributes = {
+      'aria-describedby': 'dialog-id',
+      'aria-disabled': 'false',
+      role: 'button',
+    };
+    useDraggableSpy.mockReturnValue({
+      setActivatorNodeRef: jest.fn(),
+      attributes: mockAttributes,
+    });
+
+    const { container } = render(
+      <DraggableDialogHandle>
+        <div>Handle</div>
+      </DraggableDialogHandle>
+    );
+    const handle = container.firstChild as HTMLElement;
+
+    Object.keys(mockAttributes).forEach((key) => {
+      const prop = key as keyof typeof mockAttributes;
+
+      expect(handle.getAttribute(prop)).toBe(mockAttributes[prop]);
+    });
+  });
+
   it('should throw an error if not a descendant of DraggableDialog', () => {
     render(
       <DraggableDialogHandle>
@@ -33,8 +112,58 @@ describe('DraggableDialogHandle', () => {
       </DraggableDialogHandle>
     );
 
-    expect(consoleErrorMock).toHaveBeenCalledWith(
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
       'Warning: DraggableDialogHandle is not a descendant of DraggableDialog'
     );
+  });
+
+  it('should not throw an error if a descendant of DraggableDialog', () => {
+    useDraggableDialogContextSpy.mockReturnValue({
+      hasDialogParent: true,
+    });
+
+    render(
+      <DraggableDialogHandle>
+        <div>Handle</div>
+      </DraggableDialogHandle>
+    );
+
+    expect(consoleErrorSpy).not.toHaveBeenCalled();
+  });
+
+  it('should return default values', () => {
+    useDraggableDialogContextSpy.mockReturnValue({
+      id: 'draggable-dialog-handle',
+    });
+
+    const { result } = renderHook(() => useDraggableDialogHandle());
+
+    expect(Object.keys(result.current)).toStrictEqual([
+      'setActivatorNodeRef',
+      'attributes',
+      'listeners',
+    ]);
+    ['setActivatorNodeRef', 'attributes', 'listeners'].forEach((key) => {
+      const prop = key as keyof DraggableDialogHandleState;
+
+      expect(useDraggableSpy.mock.results[0].value[prop]).toBe(
+        result.current[prop]
+      );
+    });
+  });
+
+  it('should call useDraggable with the correct id', () => {
+    const mockedId = 'draggable-dialog-handle';
+
+    useDraggableDialogContextSpy.mockReturnValue({
+      id: mockedId,
+    });
+
+    renderHook(() => useDraggableDialogHandle());
+
+    expect(useDraggableDialogContextSpy).toHaveBeenCalled();
+    expect(useDraggableSpy).toHaveBeenCalledWith({
+      id: mockedId,
+    });
   });
 });
