009e1d2848231dfa0fdb04adf6921c66f6a9083c
Marcos Moura <marcosvmmoura@gmail.com>
2024-02-19 19:39:07 +0100

    Discard changes to packages/react-draggable-dialog/src/contexts/DraggableDialogContext.ts

--- packages/react-draggable-dialog/src/contexts/DraggableDialogContext.ts
+++ packages/react-draggable-dialog/src/contexts/DraggableDialogContext.ts
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 const draggableDialogContextDefaultValue = {
-  hasDialogParent: false,
   id: '',
   isDragging: false,
   hasBeenDragged: false,
@@ -25,6 +24,8 @@ export const useDraggableDialogContext = () => {
   );
 };
 
-export const useDraggableDialogState = () => useDraggableDialogContext();
+export const useDraggableDialogState = () => {
+  return useDraggableDialogContext();
+};
 
 export const DraggableDialogContextProvider = draggableDialogContext.Provider;
