5f9598e306e31ebe5ffe0d45657adfc9b61fd524
Sean Monahan <seanmonahan@microsoft.com>
2024-03-22 16:27:50 -0700

    feat: add ThemelessFluentProvider implementation
    ThemelessFluentProvider is a replacement for FluentProvider when
    the provider needs to be rendered inside shadow DOM.

--- packages/react-themeless-provider/stories/ThemelessFluentProvider/Default.stories.tsx
+++ packages/react-themeless-provider/stories/ThemelessFluentProvider/Default.stories.tsx
@@ -1,4 +1,112 @@
 import * as React from 'react';
+import {
+  createCSSRuleFromTheme,
+  makeStyles,
+  Button,
+  Checkbox,
+  Tooltip,
+  shorthands,
+  webLightTheme,
+  FluentProvider,
+} from '@fluentui/react-components';
+import type { PartialTheme } from '@fluentui/react-components';
 import { ThemelessFluentProvider } from '@fluentui-contrib/react-themeless-provider';
+import { root } from '@fluentui-contrib/react-shadow';
 
-export const Default = () => <ThemelessFluentProvider />;
+const useClasses = makeStyles({
+  container: {
+    display: 'flex',
+    ...shorthands.gap('5px'),
+  },
+});
+
+const useShadowDomContainerStyles = makeStyles({
+  container: {
+    ...shorthands.border('2px', 'solid', 'blue'),
+    ...shorthands.padding('30px'),
+    position: 'relative',
+
+    '::before': {
+      content: '"Shadow DOM container"',
+      backgroundColor: 'blue',
+      color: 'white',
+      position: 'absolute',
+      top: '0px',
+      left: '0px',
+      fontFamily: 'sans-serif',
+      ...shorthands.padding('2px'),
+    },
+  },
+});
+
+const Container: React.FC = () => {
+  const classes = useClasses();
+
+  return (
+    <div className={classes.container}>
+      <Tooltip content="Hello, world!" relationship="label">
+        <Button>Hover me</Button>
+      </Tooltip>
+
+      <Button appearance="primary">Primary</Button>
+      <Button appearance="subtle">Subtle</Button>
+    </div>
+  );
+};
+
+const ThemeInjector: React.FC<{ theme: PartialTheme }> = ({ theme }) => {
+  const cssRule = React.useMemo(
+    () => createCSSRuleFromTheme(':root', theme),
+    [theme]
+  );
+
+  return <style dangerouslySetInnerHTML={{ __html: cssRule }} />;
+};
+
+export const Default = () => {
+  const shadowDomContainerStyles = useShadowDomContainerStyles();
+
+  const [fluentProviderEnabled, setFluentProviderEnabled] =
+    React.useState(false);
+
+  return (
+    <>
+      <p>
+        <code>ThemelessFluentProvider</code> is a replacemenet for{' '}
+        <code>FluentProvider</code> when the provider needs to be rendered
+        inside shadow DOM.
+      </p>
+      <p>
+        This example demonstrates how <code>ThemelessFluentProvider</code> works
+        when tokens are applied to <code>:root</code>.
+      </p>
+
+      <p>
+        Toggle the checkbox to enable the standard <code>FluentProvider</code>{' '}
+        to compare its behavior to <code>ThemelessFluentProvider</code> in
+        shadow DOM.
+      </p>
+
+      <FluentProvider theme={webLightTheme}>
+        <Checkbox
+          checked={fluentProviderEnabled}
+          onChange={() => setFluentProviderEnabled(!fluentProviderEnabled)}
+          label="Use standard FluentProvider"
+        />
+      </FluentProvider>
+      {!fluentProviderEnabled && <ThemeInjector theme={webLightTheme} />}
+
+      <root.div className={shadowDomContainerStyles.container}>
+        {fluentProviderEnabled ? (
+          <FluentProvider theme={webLightTheme}>
+            <Container />
+          </FluentProvider>
+        ) : (
+          <ThemelessFluentProvider>
+            <Container />
+          </ThemelessFluentProvider>
+        )}
+      </root.div>
+    </>
+  );
+};
