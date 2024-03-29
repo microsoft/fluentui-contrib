22e31390b3d08f082b25208cfd64a13fb7934081
adri <orantesadri1@gmail.com>
2024-03-29 09:06:49 -0700

    updated storybook

--- packages/azuretheme-v9/stories/brandRamp/Default.stories.tsx
+++ packages/azuretheme-v9/stories/brandRamp/Default.stories.tsx
@@ -1,4 +1,16 @@
 import * as React from 'react';
-import { brandRamp } from '@fluentui-contrib/azuretheme-v9';
+//import { brandRamp } from '@fluentui-contrib/azuretheme-v9';
+import {
+  Button,
+  FluentProvider,
+  teamsLightTheme,
+} from '@fluentui/react-components';
+import type { ButtonProps } from '@fluentui/react-components';
 
-export const Default = () => <brandRamp />;
+export const Default = (props: ButtonProps) => (
+  <FluentProvider theme={teamsLightTheme}>
+    <Button appearance="primary" {...props}>
+      Example Button
+    </Button>
+  </FluentProvider>
+);
