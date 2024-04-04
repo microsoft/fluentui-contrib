22ad5dbcd369c986ef0e693b69ff16bda399123b
Sean Monahan <seanmonahan@microsoft.com>
2024-04-04 09:27:54 -0700

    chore: scaffold react-themeless-provider package (#153)
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

---
+++ packages/react-themeless-provider/stories/ThemelessFluentProvider/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react';
+import { ThemelessFluentProvider } from '@fluentui-contrib/react-themeless-provider';
+export { Default } from './Default.stories';
+
+const meta: Meta<typeof ThemelessFluentProvider> = {
+  component: ThemelessFluentProvider,
+};
+
+export default meta;
