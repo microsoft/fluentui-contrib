dcf68f34a247300fc958f846120bd349f0f509cf
Sean Monahan <seanmonahan@microsoft.com>
2024-04-04 08:49:05 -0700

    Update packages/react-themeless-provider/stories/ThemelessFluentProvider/index.stories.tsx
    Co-authored-by: Martin Hochel <hochelmartin@gmail.com>

--- packages/react-themeless-provider/stories/ThemelessFluentProvider/index.stories.tsx
+++ packages/react-themeless-provider/stories/ThemelessFluentProvider/index.stories.tsx
@@ -1,4 +1,3 @@
-// import * as React from 'react';
 import { Meta } from '@storybook/react';
 import { ThemelessFluentProvider } from '@fluentui-contrib/react-themeless-provider';
 export { Default } from './Default.stories';
