347515ca1f4b1df0cfd749545472af25d1ebe142
Martin Hochel <hochelmartin@gmail.com>
2024-04-04 14:31:37 +0200

    chore: enable safari browser tests (#163)

--- packages/react-tree-grid/playwright.config.ts
+++ packages/react-tree-grid/playwright.config.ts
@@ -46,5 +46,9 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 });
