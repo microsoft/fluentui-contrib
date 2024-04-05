9b86f24e9cdbf9056537b0ad09cd90a38e38b9d7
adri <orantesadri1@gmail.com>
2024-04-05 12:36:37 -0700

    updated storybook

--- packages/azuretheme-v9/stories/brandRamp/Default.stories.tsx
+++ packages/azuretheme-v9/stories/brandRamp/Default.stories.tsx
@@ -5,16 +5,22 @@ import {
   azureDarkTheme,
   V9PrimaryButton,
 } from '../../src/tokens/brandRamp';
-import { FluentProvider } from '@fluentui/react-components';
+import { FluentProvider, Button } from '@fluentui/react-components';
 
 export const Default = () => (
   <div>
     <div>
       <FluentProvider theme={azureLightTheme}>
+        <Button appearance="primary">Primary</Button>
+      </FluentProvider>
+    </div>
+    <br></br>
+    <div>
+      <FluentProvider theme={azureLightTheme}>
         <V9PrimaryButton />
       </FluentProvider>
     </div>
-
+    <br></br>
     <div>
       <FluentProvider theme={azureDarkTheme}>
         <V9PrimaryButton />
