2627834da06155e1bf4c2808514b2fcdf9f6ca53
Martin Hochel <hochelmartin@gmail.com>
2024-04-09 11:19:47 +0200

    chore: remove ts-jest

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,10 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  transform: {
+    // TODO: override the nx default transform. without this we need to have ts-jest installed...
+    // @see https://github.com/nrwl/nx/blob/master/packages/jest/preset/jest-preset.ts#L8-L13
+    '^.+\\.(ts|js|html)$': ['@swc/jest'],
+  },
+};
