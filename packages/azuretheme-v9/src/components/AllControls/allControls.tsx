e17d4484b700e262626fd966fff28a5fa67117dc
Jacqueline Gaherity <30805892+Jacqueline-ms@users.noreply.github.com>
2024-05-06 11:26:48 -0700

    All basic controls added

--- packages/azuretheme-v9/src/components/AllControls/allControls.tsx
+++ packages/azuretheme-v9/src/components/AllControls/allControls.tsx
@@ -1,33 +1,50 @@
 import * as React from 'react';
-import { ButtonsExample } from '../fluent/ButtonsExample/ButtonsExample';
-import { AccordionExample } from '../fluent/AccordionExample/AccordionExample';
-import { AvatarExample } from '../fluent/AvatarExample/AvatarExample';
-import { AvatarGroupExample } from '../fluent/AvatarGroupExample/AvatarGroupExample';
-import { BadgeExample } from '../fluent/BadgeExample/BadgeExample';
-import { BreadcrumbExample } from '../fluent/BreadcrumbExample/BreadcrumbExample';
+import { ButtonsExample } from '../fluent/ButtonsExample';
+import { AccordionExample } from '../fluent/AccordionExample';
+import { AvatarExample } from '../fluent/AvatarExample';
+import { AvatarGroupExample } from '../fluent/AvatarGroupExample';
+import { BadgeExample } from '../fluent/BadgeExample';
+import { BreadcrumbExample } from '../fluent/BreadcrumbExample';
 import { CardExample } from '../fluent/CardExample';
-import { CheckboxExample } from '../fluent/CheckboxExample/CheckboxExample';
-import { ComboboxExample } from '../fluent/ComboboxExample/ComboboxExample';
-import { DataGridExample } from '../fluent/DataGridExample/DataGridExample';
-import { DiaglogExample } from '../fluent/DialogExample/DialogExample';
-import { DividerExample } from '../fluent/DividerExample/DividerExample';
-import { DrawerExample } from '../fluent/DrawerExample/DrawerExample';
-import { DropdownExample } from '../fluent/DropdownExample/DropdownExample';
-import { FieldExample } from '../fluent/FieldExample/FieldExample';
-import { InfoLabelExample } from '../fluent/InfoLabelExample/InfoLabelExample';
-import { InputExample } from '../fluent/InputExample/InputExample';
-import { LabelExample } from '../fluent/LabelExample/LabelExample';
-import { LinkExample } from '../fluent/LinkExample/LinkExample';
-import { MenuExample } from '../fluent/MenuExample/MenuExample';
-import { MenuListExample } from '../fluent/MenuListExample/MenuListExample';
-import { MessageBarExample } from '../fluent/MessageBarExample/MessageBarExample';
-import { OverflowExample } from '../fluent/OverflowExample/OverflowExample';
-import { PersonaExample } from '../fluent/PersonaExample/PersonaExample';
-import { PopoverExample } from '../fluent/PopoverExample/PopoverExample';
-import { ProgressBarExample } from '../fluent/ProgressBarExample/ProgressBarExample';
-import { RadioGroupExample } from '../fluent/RadioGroupExample/RadioGroupExample';
-// import { SearchBoxExample } from '../fluent/SearchBoxExample/SearchBoxExample';
-
+import { CheckboxExample } from '../fluent/CheckboxExample';
+import { ComboboxExample } from '../fluent/ComboboxExample';
+import { DataGridExample } from '../fluent/DataGridExample';
+import { DiaglogExample } from '../fluent/DialogExample';
+import { DividerExample } from '../fluent/DividerExample';
+import { DrawerExample } from '../fluent/DrawerExample';
+import { DropdownExample } from '../fluent/DropdownExample';
+import { FieldExample } from '../fluent/FieldExample';
+import { InfoLabelExample } from '../fluent/InfoLabelExample';
+import { InputExample } from '../fluent/InputExample';
+import { LabelExample } from '../fluent/LabelExample';
+import { LinkExample } from '../fluent/LinkExample';
+import { MenuExample } from '../fluent/MenuExample';
+import { MenuListExample } from '../fluent/MenuListExample';
+import { MessageBarExample } from '../fluent/MessageBarExample';
+import { OverflowExample } from '../fluent/OverflowExample';
+import { PersonaExample } from '../fluent/PersonaExample';
+import { PopoverExample } from '../fluent/PopoverExample';
+import { ProgressBarExample } from '../fluent/ProgressBarExample';
+import { RadioGroupExample } from '../fluent/RadioGroupExample';
+import { SearchBoxExample } from '../fluent/SearchBoxExample';
+import { SelectExample } from '../fluent/SelectExample';
+import { SkeletonExample } from '../fluent/SkeletonExample';
+import { SliderExample } from '../fluent/SliderExample';
+import { SpinButtonExample } from '../fluent/SpinButtonExample';
+import { SpinnerExample } from '../../../../../SpinnerExample';
+import { SwitchExample } from '../fluent/SwitchExample';
+import { TableExample } from '../fluent/TableExample';
+import { TabeListExample } from '../fluent/TabListExample';
+import { InteractionTagExample } from '../fluent/TagExamples/InteractionTag';
+import { TagExample } from '../fluent/TagExamples/Tag';
+import { TagGroupExample } from '../fluent/TagExamples/TagGroup';
+import { TeachingPopoverExample } from '../fluent/TeachingPopover';
+import { TextExample } from '../fluent/Text';
+// import { TextareaExample } from '../fluent/TextareaExample';
+import { ToastExample } from '../fluent/Toast';
+import { ToolbarExample } from '../fluent/Toolbar';
+import { TooltipExample } from '../fluent/Tooltip';
+import { TreeExample } from '../fluent/Tree';
 
 export const AllControls = () => {
   return (
@@ -60,7 +77,25 @@ export const AllControls = () => {
       <PopoverExample />
       <ProgressBarExample />
       <RadioGroupExample />
-      {/* <SearchBoxExample /> */}
+      <SearchBoxExample />
+      <SelectExample />
+      <SkeletonExample />
+      <SliderExample />
+      <SpinButtonExample />
+      <SpinnerExample />
+      <SwitchExample />
+      <TableExample />
+      <TabeListExample />
+      <InteractionTagExample />
+      <TagExample />
+      <TagGroupExample />
+      <TeachingPopoverExample />
+      <TextExample />
+      {/* <TextareaExample /> */}
+      <ToastExample />
+      <ToolbarExample />
+      <TooltipExample />
+      <TreeExample />
     </div>
   );
 };
--- packages/azuretheme-v9/stories/darkTheme/Default.stories.tsx
+++ packages/azuretheme-v9/stories/darkTheme/Default.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { azureDarkTheme } from '../../src/tokens/brandRamp';
 import { FluentProvider } from '@fluentui/react-components';
-import { AllControls } from '../../src/components/AllControls/allControls';
+import { AllControls } from '../../src/components/AllControls/AllControls';
 export const Default = () => (
   <FluentProvider theme={azureDarkTheme}>
     <div>Dark theme </div>
