217ebc670d0340bd5526ebd2a925fcaf8a77d3e0
adri <orantesadri1@gmail.com>
2024-05-15 09:04:17 -0700

    added export for azure light and dark themes

--- packages/azuretheme-v9/src/index.ts
+++ packages/azuretheme-v9/src/index.ts
@@ -1,3 +1,5 @@
 export * from './components/highContrast';
 export * from './components/darkTheme';
 export * from './components/lightTheme';
+export { azureLightTheme } from './tokens/brandRamp';
+export { azureDarkTheme } from './tokens/brandRamp';
