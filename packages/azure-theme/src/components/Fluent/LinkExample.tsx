9eb8560861b805b5c559a51578ca569ac697f00f
adri <orantesadri1@gmail.com>
2024-05-21 14:40:28 -0700

    added controls

---
+++ packages/azure-theme/src/components/Fluent/LinkExample.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "@fluentui/react-components";
+import type { LinkProps } from "@fluentui/react-components";
+
+export const LinkExample = (props: LinkProps & { as?: "a" }) => (
+    <>
+    <Link href="https://www.bing.com" {...props}>
+        This is a link
+    </Link>
+        <Link disabled href="https://www.bing.com">
+        Disabled link
+    </Link>
+  </>
+);
---
+++ packages/azure-theme/src/components/Fluent/RadioGroupExample.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { Field, Radio, RadioGroup, Text } from "@fluentui/react-components";
+
+export const RadioGroupExample = () => (
+  <Field label="Favorite Fruit">
+    <RadioGroup defaultValue="apple">
+      <Radio value="apple" label="Apple" />
+      <Radio
+        value="B"
+        label={
+          <>
+            Pear
+            <br />
+            <Text size={200}>This is some more example subtext</Text>
+          </>
+        }
+      />
+      <Radio value="banana" label="Banana" disabled />
+      <Radio value="orange" label="Orange" />
+     
+    </RadioGroup>
+  </Field>
+);
