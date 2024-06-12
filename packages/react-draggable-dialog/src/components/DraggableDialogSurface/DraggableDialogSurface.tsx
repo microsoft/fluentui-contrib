58b83358ac4bc6ee7b7478ab6bd60caedc814662
torlye <torarnelye+git@gmail.com>
2024-06-12 12:41:43 +0200

    feat(react-draggable-dialog): add ref forwarding for DraggableDialogSurface (#185)

--- packages/react-draggable-dialog/src/components/DraggableDialogSurface/DraggableDialogSurface.tsx
+++ packages/react-draggable-dialog/src/components/DraggableDialogSurface/DraggableDialogSurface.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { DialogSurface, mergeClasses } from '@fluentui/react-components';
+import {
+  DialogSurface,
+  ForwardRefComponent,
+  mergeClasses,
+} from '@fluentui/react-components';
 
 import { DraggableDialogSurfaceProps } from './DraggableDialogSurface.types';
 import { useStyles } from './DraggableDialogSurface.styles';
@@ -9,27 +13,28 @@ import { useDraggableDialogSurface } from './useDraggableDialogSurface';
  * DraggableDialogSurface is a wrapper around the DialogSurface component that,
  * when composed with DraggableDialog, can be dragged.
  */
-export const DraggableDialogSurface: React.FC<DraggableDialogSurfaceProps> = ({
-  children,
-  className,
-  ...props
-}) => {
-  const styles = useStyles();
-  const { ref, style, mountNode } = useDraggableDialogSurface(props);
+export const DraggableDialogSurface: ForwardRefComponent<DraggableDialogSurfaceProps> =
+  React.forwardRef((props, forwardedRef) => {
+    const { children, className } = props;
+    const styles = useStyles();
+    const { ref, style, mountNode } = useDraggableDialogSurface(
+      props,
+      forwardedRef
+    );
 
-  return (
-    <DialogSurface
-      ref={ref}
-      style={style}
-      className={mergeClasses(
-        'fui-DraggableDialogSurface',
-        styles.root,
-        className
-      )}
-      mountNode={mountNode}
-      {...props}
-    >
-      {children}
-    </DialogSurface>
-  );
-};
+    return (
+      <DialogSurface
+        ref={ref}
+        style={style}
+        className={mergeClasses(
+          'fui-DraggableDialogSurface',
+          styles.root,
+          className
+        )}
+        mountNode={mountNode}
+        {...props}
+      >
+        {children}
+      </DialogSurface>
+    );
+  });
