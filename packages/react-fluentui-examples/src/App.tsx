c955230afd37100a28f7bccbd1359555795f7537
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2024-06-27 11:14:31 -0700

    add react-components

--- packages/react-fluentui-examples/src/App.tsx
+++ packages/react-fluentui-examples/src/App.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Button } from '@fluentui/react-components';
+
 function App() {
-  return <h1>Welcome to Fluent UI V9 Example site</h1>;
+  return (
+    <>
+      <h1>Welcome to Fluent UI V9 Example site</h1>
+      <Button
+        appearance="primary"
+        onClick={() => alert("This doesn't actually do anything yet :)")}
+      >
+        Get started!
+      </Button>
+    </>
+  );
 }
 
 export default App;
