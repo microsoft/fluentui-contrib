767a884e1b61d9e84e9018546294d2d5537bd72f
Adriana Orantes <aorantes@Adrianas-MacBook-Pro.local>
2024-06-27 14:12:24 -0700

    added tokens (brand ramp and neutral colors) and created light and dark theme components

---
+++ packages/azure-theme/src/AzureDarkTheme.tsx
@@ -0,0 +1,10 @@
+import { Theme, createDarkTheme } from '@fluentui/react-components';
+import { brandAzure } from './tokens/brandRamp';
+import { neutralColorsDark } from './tokens/brandRamp';
+
+const darkColors: Theme = createDarkTheme(brandAzure);
+
+export const AzureLightTheme: Theme = {
+  ...darkColors,
+  ...neutralColorsDark,
+};
---
+++ packages/azure-theme/src/AzureLightTheme.tsx
@@ -0,0 +1,10 @@
+import { Theme, createLightTheme } from '@fluentui/react-components';
+import { brandAzure } from './tokens/brandRamp';
+import { neutralColorsLight } from './tokens/brandRamp';
+
+const lightColors: Theme = createLightTheme(brandAzure);
+
+export const azureLightTheme: Theme = {
+  ...lightColors,
+  ...neutralColorsLight,
+};
