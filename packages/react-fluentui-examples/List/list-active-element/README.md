76a1d47981274faf2d26d19e554fb30c1038f1d2
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2024-06-27 17:15:30 -0700

    package in each example

---
+++ packages/react-fluentui-examples/List/list-active-element/README.md
@@ -0,0 +1,3 @@
+# Fluent UI React V9 - List - Active Element
+
+This example shows how a List component can be used to visualize an active element.
--- packages/react-fluentui-examples/List/list-active-element/src/main.tsx
+++ packages/react-fluentui-examples/List/list-active-element/src/main.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
-import List from './examples/List/';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-
-  {
-    path: '/list',
-    element: <List />,
-  },
-]);
+import Example from './List-Active-Element.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FluentProvider theme={webLightTheme}>
-      <RouterProvider router={router} />
+      <Example />
     </FluentProvider>
   </React.StrictMode>
 );
