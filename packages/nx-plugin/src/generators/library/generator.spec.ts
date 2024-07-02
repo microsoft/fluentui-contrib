ec7bad965c95217517bba5b215cbb4fcb5e385c1
Martin Hochel <hochelmartin@gmail.com>
2024-07-02 17:05:15 +0200

    test: update snapshots

--- packages/nx-plugin/src/generators/library/generator.spec.ts
+++ packages/nx-plugin/src/generators/library/generator.spec.ts
@@ -76,7 +76,7 @@ describe('create-package generator', () => {
         }
 
         // Uncomment if using global setup/teardown files being transformed via swc
-        // https://nx.dev/packages/jest/documents/overview#global-setup/teardown-with-nx-libraries
+        // https://nx.dev/nx-api/jest/documents/overview#global-setupteardown-with-nx-libraries
         // jest needs EsModule Interop to find the default exported setup/teardown functions
         // swcJestConfig.module.noInterop = false;
 
