fcaa1506a3f0e3a8d428d066c74491430a8644b7
Martin Hochel <hochelmartin@gmail.com>
2024-07-02 16:43:03 +0200

    test(nx-plugin): add coverage for component generator

--- packages/nx-plugin/src/generators/component/generator.spec.ts
+++ packages/nx-plugin/src/generators/component/generator.spec.ts
@@ -1,5 +1,11 @@
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nx/devkit';
+import { Tree, joinPathFragments, readProjectConfiguration } from '@nx/devkit';
+
+import libraryGenerator from '../library/generator';
+import {
+  createCodeowners,
+  setupWorkspaceDependencies,
+} from '../../utils-testing';
 
 import generator from './generator';
 import { ComponentGeneratorSchema } from './schema';
@@ -7,17 +13,40 @@ import { ComponentGeneratorSchema } from './schema';
 describe('component generator', () => {
   let tree: Tree;
   const options: ComponentGeneratorSchema = {
-    name: 'test',
+    name: 'hello-world',
     componentName: 'foo',
   };
 
-  beforeEach(() => {
+  beforeEach(async () => {
     tree = createTreeWithEmptyWorkspace();
+    setupWorkspaceDependencies(tree);
+    createCodeowners(tree);
+    await libraryGenerator(tree, { name: options.name, owner: '@MrWick' });
   });
 
-  xit('should run successfully', async () => {
+  it('should create component with story', async () => {
     await generator(tree, options);
-    const config = readProjectConfiguration(tree, 'test');
-    expect(config).toBeDefined();
+    const config = readProjectConfiguration(tree, options.name);
+
+    expect(
+      tree.children(
+        joinPathFragments(config.sourceRoot as string, 'components/foo')
+      )
+    ).toMatchInlineSnapshot(`
+      [
+        "foo.styles.ts",
+        "foo.test.tsx",
+        "foo.tsx",
+        "index.ts",
+      ]
+    `);
+
+    expect(tree.children(joinPathFragments(config.root, 'stories/foo')))
+      .toMatchInlineSnapshot(`
+      [
+        "Default.stories.tsx",
+        "index.stories.tsx",
+      ]
+    `);
   });
 });
