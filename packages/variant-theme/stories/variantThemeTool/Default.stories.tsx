f30321fdb078ffd04d526e46571a8d6a2fdd550d
wenyaoj2 <wenyaoj2@illinois.edu>
2024-07-19 15:38:34 +0800

    remove logic from stories

--- packages/variant-theme/stories/variantThemeTool/Default.stories.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { variantThemeTool } from '@fluentui-contrib/variant-theme';
-
-export const Default = () => <variantThemeTool />;
--- packages/variant-theme/stories/variantThemeTool/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta } from '@storybook/react';
-import { variantThemeTool } from '@fluentui-contrib/variant-theme';
-export { Default } from './Default.stories';
-
-const meta: Meta<typeof variantThemeTool> = {
-  component: variantThemeTool,
-};
-
-export default meta;
