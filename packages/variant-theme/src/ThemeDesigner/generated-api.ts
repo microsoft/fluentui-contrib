3d4e2a80674c9fac1c6dffa6c82c3cfc25823f27
wenyaoj2 <wenyaoj2@illinois.edu>
2024-08-13 15:35:51 +0800

    remove extra yarn.lock

--- packages/variant-theme/src/ThemeDesigner/generated-api.ts
+++ packages/variant-theme/src/ThemeDesigner/generated-api.ts
@@ -1,5 +1,2 @@
-export * from './utils/getOverridableTokenBrandColors';
-export * from './utils/brandColors';
-export * from './utils/getBrandTokensFromPalette';
 export { hex_to_sRGB } from './colors/palettes';
 export { contrast } from './colors/csswg';
