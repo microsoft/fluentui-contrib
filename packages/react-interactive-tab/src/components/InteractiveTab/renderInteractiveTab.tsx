219813601e6cd55a53ccf3f53ea61968ab1e9a19
Dmytro Kirpa <kirpadv@gmail.com>
2024-09-11 11:43:23 +0200

    review comments

--- packages/react-interactive-tab/src/components/InteractiveTab/renderInteractiveTab.tsx
+++ packages/react-interactive-tab/src/components/InteractiveTab/renderInteractiveTab.tsx
@@ -1,9 +1,4 @@
-/** @jsxRuntime classic */
-/** @jsx createElement */
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { createElement } from '@fluentui/react-jsx-runtime';
-
+import * as React from 'react';
 import { assertSlots } from '@fluentui/react-components';
 import type {
   InteractiveTabState,
