b4ec441dd754bd5c8cfaf36bd8ce75a561bf1660
Adriana Orantes <aorantes@adrianas-mbp.mynetworksettings.com>
2024-09-19 09:36:09 -0700

    Revert "Added test for Azure Light theme"
    This reverts commit 491fe49f5a95ec930bc73987844e20d318edeab8.

--- packages/azure-theme/src/components/AzureLightTheme/AzureLightTheme.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { AzureLightTheme } from './AzureLightTheme';
-import { FluentProvider } from '@fluentui/react-components';
-describe('AzureLightTheme', () => {
-  it('should render', () => {
-    render(<FluentProvider theme={AzureLightTheme}></FluentProvider>);
-  });
-});
