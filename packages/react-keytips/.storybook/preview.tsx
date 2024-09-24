162ff398c02395a563488559a6b40f69fb72e481
mainframev <vgenaev@gmail.com>
2024-09-24 13:36:27 +0200

    docs(react-keytips): add sb themes addon

--- packages/react-keytips/.storybook/preview.tsx
+++ packages/react-keytips/.storybook/preview.tsx
@@ -1,16 +1,28 @@
-import * as React from 'react';
-
 import { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+  teamsLightTheme,
+  teamsDarkTheme,
+  teamsHighContrastTheme,
+} from '@fluentui/react-components';
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <FluentProvider theme={webLightTheme}>
-        <Story />
-      </FluentProvider>
-    ),
+    withThemeFromJSXProvider({
+      themes: {
+        webLightTheme: webLightTheme,
+        webDarkTheme: webDarkTheme,
+        teamsLightTheme: teamsLightTheme,
+        teamsDarkTheme: teamsDarkTheme,
+        teamsHighContrastTheme: teamsHighContrastTheme,
+      },
+      defaultTheme: 'webLightTheme',
+      Provider: FluentProvider,
+    }),
   ],
 };
 
