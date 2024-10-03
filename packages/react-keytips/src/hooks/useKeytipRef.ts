63c29213d034e21428dbd3f84c1e2b1ccca2c717
mainframev <vgenaev@gmail.com>
2024-10-03 15:06:00 +0200

    fix(react-keytips): useKeytipRef, adjust type

--- packages/react-keytips/src/hooks/useKeytipRef.ts
+++ packages/react-keytips/src/hooks/useKeytipRef.ts
@@ -8,22 +8,26 @@ const isEqualArray = (a: string[], b: string[]) => {
   return a.length === b.length && a.every((v, i) => v === b[i]);
 };
 
-export const useKeytipRef = <T extends HTMLElement = HTMLButtonElement>(
+export const useKeytipRef = <
+  T extends HTMLElement = HTMLButtonElement | HTMLAnchorElement
+>(
   keytip: Omit<KeytipProps, 'uniqueId'>
-) => {
+): React.Dispatch<React.SetStateAction<T | null>> => {
   const uniqueId = React.useId();
-
-  const [node, setNode] = React.useState<T | undefined>();
+  const [node, setNode] = React.useState<T | null>(null);
   const { dispatch } = useEventService();
 
-  const ktp = {
-    ...keytip,
-    uniqueId,
-    positioning: {
-      target: node,
-      ...keytip.positioning,
-    },
-  };
+  const ktp = React.useMemo(
+    () => ({
+      ...keytip,
+      uniqueId,
+      positioning: {
+        target: node,
+        ...keytip.positioning,
+      },
+    }),
+    [keytip, uniqueId, node]
+  );
 
   const prevKeytip = usePrevious(ktp);
 
