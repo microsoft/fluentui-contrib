9a087c34772c5e3ab481e4fbb28966781281e692
Dmytro Kirpa <kirpadv@gmail.com>
2024-10-15 18:39:24 +0200

    review comments

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,10 +3,10 @@ import type { Preview } from '@storybook/react';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 // TODO: import components and styles from the Fluent Storybook Addon when it is published
-import { FluentDocsContainer } from './components/FluentDocsContainer';
-import { FluentDocsPage } from './components/FluentDocsPage';
-import './docs-root.css';
-import './docs-root-v9.css';
+import { FluentDocsContainer } from './copied-from-fluentui-core/components/FluentDocsContainer';
+import { FluentDocsPage } from './copied-from-fluentui-core/components/FluentDocsPage';
+import './copied-from-fluentui-core/docs-root.css';
+import './copied-from-fluentui-core/docs-root-v9.css';
 
 const preview: Preview = {
   decorators: [
