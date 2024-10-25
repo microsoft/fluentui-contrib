b628806af8b02d2d73fc0873cff5dabd26f4cf42
mainframev <vgenaev@gmail.com>
2024-10-25 23:38:24 +0200

    fix(react-keytips): improve exports

--- packages/react-keytips/src/index.ts
+++ packages/react-keytips/src/index.ts
@@ -1,8 +1,25 @@
 export {
   Keytip,
+  useKeytip_unstable,
+  useKeytipStyles_unstable,
+  renderKeytip_unstable,
+  keytipClassNames,
+} from './Keytip';
+
+export type {
   KeytipProps,
+  KeytipSlots,
+  KeytipState,
   ReturnKeytipEventHandler,
   ExecuteKeytipEventHandler,
-} from './components/Keytip';
-export { Keytips, KeytipsProps } from './components/Keytips';
+} from './Keytip';
+
+export {
+  Keytips,
+  renderKeytips_unstable,
+  useKeytips_unstable,
+} from './components/Keytips';
+
+export type { KeytipsProps, KeytipsSlots, KeytipsState } from './Keytips';
+
 export { useKeytipRef } from './hooks/useKeytipRef';
