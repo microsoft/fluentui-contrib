6dc4697518508ccdb3d54420f05bd7ed8529809b
mainframev <vgenaev@gmail.com>
2024-10-30 13:48:37 +0100

    docs(react-keytips): improve keytips for docsite

--- packages/react-keytips/stories/index.stories.tsx
+++ packages/react-keytips/stories/index.stories.tsx
@@ -1,5 +1,8 @@
+import * as React from 'react';
+
 import { Meta } from '@storybook/react';
 import { Keytips } from '@fluentui-contrib/react-keytips';
+import description from '../README.md';
 export { DefaultStory as Default } from './Default.stories';
 export { WithTabsStory as WithTabs } from './WithTabs.stories';
 export { DynamicStory as Dynamic } from './Dynamic.stories';
@@ -8,6 +11,22 @@ export { OverflowStory as Overflow } from './OverflowMenu.stories';
 const meta = {
   title: 'Packages/react-keytips',
   component: Keytips,
+  decorators: [
+    (Story, { viewMode, name }) => (
+      <>
+        {viewMode === 'story' && <Keytips />}
+        {viewMode === 'docs' && name === 'Default' && <Keytips />}
+        <Story />
+      </>
+    ),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
 } satisfies Meta<typeof Keytips>;
 
 export default meta;
