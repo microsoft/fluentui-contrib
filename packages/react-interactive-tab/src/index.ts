2dbb995fb5b366b15a5134b578f10042aa309a89
Sonia Boller <soniaboller@gmail.com>
2024-10-31 14:37:50 -0700

    refactor: export all types and hooks from barrel file

--- packages/react-interactive-tab/src/index.ts
+++ packages/react-interactive-tab/src/index.ts
@@ -1,2 +1,13 @@
-export { InteractiveTab } from './components/InteractiveTab';
-export {};
+export type {
+  InteractiveTabProps,
+  InteractiveTabSlots,
+  InteractiveTabState,
+  InteractiveTabInternalSlots,
+} from './components/InteractiveTab';
+export {
+  InteractiveTab,
+  interactiveTabClassNames,
+  renderInteractiveTab_unstable,
+  useInteractiveTabStyles_unstable,
+  useInteractiveTab_unstable,
+} from './components/InteractiveTab';
