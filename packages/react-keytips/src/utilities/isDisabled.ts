82bb91bbc86ee62197b89f8a074917b8fc3a586a
Victor Genaev <vgenaev@gmail.com>
2024-11-05 23:40:17 +0100

    Update packages/react-keytips/src/utilities/isDisabled.ts
    Co-authored-by: Dmytro Kirpa <kirpadv@gmail.com>

--- packages/react-keytips/src/utilities/isDisabled.ts
+++ packages/react-keytips/src/utilities/isDisabled.ts
@@ -2,7 +2,7 @@ export const isDisabled = (target?: HTMLElement): boolean => {
   if (!target) return false;
 
   return (
-    target?.hasAttribute('disabled') ||
-    target?.getAttribute('aria-disabled')?.toLowerCase() === 'true'
+    target.hasAttribute('disabled') ||
+    target.getAttribute('aria-disabled')?.toLowerCase() === 'true'
   );
 };
