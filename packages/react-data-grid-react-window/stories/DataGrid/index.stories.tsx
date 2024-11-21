55927b7da4b470202c356fce0668625098932269
Reuben Rakete <reubenrakete@microsoft.com>
2024-11-20 16:04:38 -0800

    Add story for resizable data grid

--- packages/react-data-grid-react-window/stories/DataGrid/index.stories.tsx
+++ packages/react-data-grid-react-window/stories/DataGrid/index.stories.tsx
@@ -5,6 +5,7 @@ import description from '../../README.md';
 export { VirtualizedDataGrid } from './VirtualizedDataGrid.stories';
 export { ReactWindowOverrides } from './ReactWindowOverrides.stories';
 export { DataGridScrollingIndicators } from './DataGridScrollingIndicators.stories';
+export { ResizableDataGrid } from './ResizableDataGrid.stories';
 
 const meta: Meta<typeof DataGrid> = {
   title: 'Packages/react-data-grid-react-window',
