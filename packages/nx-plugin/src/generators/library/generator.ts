10ebd5ac44c93e4f13a451aa59b9af9719a4563c
Martin Hochel <hochelmartin@gmail.com>
2025-01-20 17:18:53 +0100

    fix(nx-plugin): remove unwatned @nx/js release boilerplate from library generator output

--- packages/nx-plugin/src/generators/library/generator.ts
+++ packages/nx-plugin/src/generators/library/generator.ts
@@ -7,6 +7,10 @@ import {
   generateFiles,
   readProjectConfiguration,
   readJson,
+  joinPathFragments,
+  ProjectConfiguration,
+  readNxJson,
+  updateNxJson,
 } from '@nx/devkit';
 import * as path from 'path';
 import { libraryGenerator } from '@nx/js';
@@ -17,9 +21,7 @@ import { addCodeowner } from '../add-codeowners';
 export default async function (tree: Tree, options: LibraryGeneratorSchema) {
   const { name, owner } = options;
 
-  await invokeNxGenerators(tree, options);
-
-  const newProject = readProjectConfiguration(tree, name);
+  const { projectConfig: newProject } = await invokeNxGenerators(tree, options);
 
   const { root: projectRoot } = newProject;
   const paths = getPackagePaths(workspaceRoot, projectRoot);
@@ -90,6 +92,7 @@ function getReactComponentsVersion(tree: Tree) {
 
 async function invokeNxGenerators(tree: Tree, options: LibraryGeneratorSchema) {
   const { name } = options;
+  const currentNxJson = readNxJson(tree);
 
   await libraryGenerator(tree, {
     name,
@@ -102,7 +105,42 @@ async function invokeNxGenerators(tree: Tree, options: LibraryGeneratorSchema) {
     importPath: `${npmScope}/${name}`,
   });
 
+  const projectConfig = readProjectConfiguration(tree, name);
+
+  // remove nx release implicit config @see https://github.com/nrwl/nx/blob/master/packages/js/src/generators/library/library.ts#L341-L356
+  if (projectConfig.release) {
+    delete projectConfig.release;
+  }
+  if (projectConfig.targets?.['nx-release-publish']) {
+    delete projectConfig.targets?.['nx-release-publish'];
+  }
+  updateProjectConfiguration(tree, name, { ...projectConfig });
+
   // remove nx/jest generator defaults that we don't need
+  updateJson(
+    tree,
+    joinPathFragments(projectConfig.root, 'project.json'),
+    (json: ProjectConfiguration) => {
+      if (json.release) {
+        delete json.release;
+      }
+      if (json.targets?.['nx-release-publish']) {
+        delete json.targets?.['nx-release-publish'];
+      }
+
+      return json;
+    }
+  );
+
+  // remove unwanted release.version.preVersionCommand override @see https://github.com/nrwl/nx/blob/master/packages/js/src/generators/library/library.ts#L1252
+  const nxJson = readNxJson(tree);
+  if (currentNxJson?.release?.version) {
+    nxJson.release.version = currentNxJson.release.version;
+  } else {
+    delete nxJson?.release?.version;
+  }
+  updateNxJson(tree, nxJson);
+
   updateJson(tree, '/package.json', (json: PackageJson) => {
     if (json.devDependencies) {
       // @see https://github.com/nrwl/nx/blob/master/packages/jest/src/generators/configuration/lib/ensure-dependencies.ts#L26
@@ -113,5 +151,5 @@ async function invokeNxGenerators(tree: Tree, options: LibraryGeneratorSchema) {
     return json;
   });
 
-  return tree;
+  return { tree, projectConfig };
 }
