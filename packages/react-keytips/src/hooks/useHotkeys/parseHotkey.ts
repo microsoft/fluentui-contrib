5ef192c809fb321c5edc73862721208f9d135a87
mainframev <vgenaev@gmail.com>
2025-02-01 21:50:37 +0100

    refactor(react-keytips): refactor useHotkeys

---
+++ packages/react-keytips/src/hooks/useHotkeys/parseHotkey.ts
@@ -0,0 +1,20 @@
+const MODIFIERS = ['shift', 'alt', 'control', 'meta', 'mod'];
+
+export function parseHotkey(hotkey: string) {
+  const keys = hotkey.toLowerCase().split('+');
+
+  const modifiers = {
+    alt: keys.includes('alt'),
+    ctrl: keys.includes('ctrl') || keys.includes('control'),
+    shift: keys.includes('shift'),
+    meta: keys.includes('meta'),
+    mod: keys.includes('mod'),
+  };
+
+  const key = keys.find((k) => !MODIFIERS.includes(k));
+
+  return {
+    ...modifiers,
+    key,
+  };
+}
