9c4c4a9636fc6af55bcfac9704485e688a4961df
Hector Jimenez <hectorjimenez@outlook.com>
2025-02-05 15:52:51 -0800

    nx publish target

--- packages/react-gamepad-navigation/src/index.ts
+++ packages/react-gamepad-navigation/src/index.ts
@@ -1,3 +1,9 @@
 export { useGamepadNavigationGroup } from './hooks/useGamepadNavigationGroup';
+export {
+  initGamepadNavigation,
+  deinitGamepadNavigation,
+} from './core/GamepadNavigation';
 export type { UseGamepadNavigationGroupOptions } from './hooks/useGamepadNavigationGroup';
+export type { FocusFinderFunctions } from './types/FocusFinders';
+export type { GamepadNavigationProps } from './core/GamepadNavigation';
 export type { TabsterDOMAttribute } from '@fluentui/react-tabster';
