b515dea0d81637675bc05ca1985294d9d19b6d85
Hector Jimenez <hectorjimenez@outlook.com>
2025-02-20 08:34:38 -0800

    addressing comments

--- packages/react-gamepad-navigation/src/types/FocusFinders.ts
+++ packages/react-gamepad-navigation/src/types/FocusFinders.ts
@@ -1,18 +1,3 @@
-export type FocusFinderFunctions = {
-  findAllFocusable: (
-    container: HTMLElement,
-    acceptCondition?: ((el: HTMLElement) => boolean) | undefined
-  ) => HTMLElement[];
-  findFirstFocusable: (
-    container: HTMLElement
-  ) => HTMLElement | null | undefined;
-  findLastFocusable: (container: HTMLElement) => HTMLElement | null | undefined;
-  findNextFocusable: (
-    currentElement: HTMLElement,
-    options?: { container?: HTMLElement | undefined }
-  ) => HTMLElement | null | undefined;
-  findPrevFocusable: (
-    currentElement: HTMLElement,
-    options?: { container?: HTMLElement | undefined }
-  ) => HTMLElement | null | undefined;
-};
+import { useFocusFinders } from '@fluentui/react-components';
+
+export type FocusFinderFunctions = ReturnType<typeof useFocusFinders>;
