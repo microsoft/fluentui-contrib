9b351c13ed3b5a213737d9cac807a8cd2484dfe7
Hector Jimenez <6352233+hectorjjb@users.noreply.github.com>
2025-02-27 09:53:42 -0800

    feat: react-gamepad-navigation implementation (#286)
    Co-authored-by: Marcos Moura <marcosvmmoura@gmail.com>
    Co-authored-by: Dmytro Kirpa <kirpadv@gmail.com>

---
+++ packages/react-gamepad-navigation/src/core/GamepadUtils.ts
@@ -0,0 +1,91 @@
+import { WindowWithFluentGPNShadowDOMAPI } from '../types/FluentGPNShadowDOMAPI';
+import { KeyboardKey, MoverKey, MoverKeys } from '../types/Keys';
+import { selectOptionsVisibleAttribute } from './Constants';
+import { emitSyntheticKeyboardEvent } from './GamepadEvents';
+import { getMoverKeyToKeyboardKeyMapping } from './GamepadMappings';
+
+export type TimeoutId = number | undefined;
+export type IntervalId = number | undefined;
+
+export const shouldSubmitForm = (element: Element | null | undefined) =>
+  element instanceof HTMLInputElement &&
+  (element.type === 'password' ||
+    element.type === 'text' ||
+    element.type === 'email' ||
+    element.type === 'tel');
+
+export const isComboboxElement = (element: Element | null | undefined) => {
+  return element?.getAttribute('role') === 'combobox';
+};
+
+export const isMenuItemElement = (element: Element | null | undefined) => {
+  return element?.getAttribute('role') === 'menuitem';
+};
+
+export const isRadioElement = (element: Element | null | undefined) => {
+  return element?.getAttribute('type') === 'radio';
+};
+
+export const isSelectElement = (element: Element | null | undefined) => {
+  return element?.tagName === 'SELECT';
+};
+
+export const hidePickerOnSeLectElement = (selectElement: HTMLSelectElement) => {
+  selectElement.blur();
+  selectElement.focus();
+  selectElement.removeAttribute(selectOptionsVisibleAttribute);
+};
+
+export const handleSelectOnEnter = (activeElement: Element | null) => {
+  const htmlSelect = activeElement as HTMLSelectElement;
+  const openOptions = htmlSelect.hasAttribute(selectOptionsVisibleAttribute);
+  if (openOptions) {
+    hidePickerOnSeLectElement(htmlSelect);
+  } else {
+    htmlSelect.showPicker();
+    htmlSelect.setAttribute(selectOptionsVisibleAttribute, '');
+  }
+};
+
+export const handleSelectOnEscape = (targetDocument: Document) => {
+  const htmlSelect = targetDocument.activeElement as HTMLSelectElement;
+  const openOptions = htmlSelect.hasAttribute(selectOptionsVisibleAttribute);
+  if (openOptions) {
+    hidePickerOnSeLectElement(htmlSelect);
+  } else {
+    emitSyntheticKeyboardEvent(
+      'keydown',
+      KeyboardKey.Escape,
+      true,
+      targetDocument
+    );
+  }
+};
+
+export const handleSelectOnDirection = (
+  targetDocument: Document,
+  key: MoverKey
+) => {
+  const htmlSelect = targetDocument.activeElement as HTMLSelectElement;
+  const openOptions = htmlSelect.hasAttribute(selectOptionsVisibleAttribute);
+
+  // TODO: account for navigation to active select sibling elements vs options
+  if (openOptions && key === MoverKeys.ArrowDown) {
+    if (htmlSelect.selectedIndex < htmlSelect.options.length - 1) {
+      htmlSelect.selectedIndex++;
+    }
+  } else if (openOptions && key === MoverKeys.ArrowUp) {
+    if (htmlSelect.selectedIndex > 0) {
+      htmlSelect.selectedIndex--;
+    }
+  } else if (!openOptions) {
+    const button = getMoverKeyToKeyboardKeyMapping(key);
+    emitSyntheticKeyboardEvent('keydown', button, true, targetDocument);
+  }
+};
+export const getShadowDOMAPI = (targetDocument: Document | undefined) => {
+  const defaultView = targetDocument?.defaultView;
+  const shadowDOMAPI = (defaultView as WindowWithFluentGPNShadowDOMAPI)
+    ?.__FluentGPNShadowDOMAPI;
+  return shadowDOMAPI;
+};
--- packages/react-gamepad-navigation/src/index.ts
+++ packages/react-gamepad-navigation/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { useGamepadNavigationGroup } from './hooks/useGamepadNavigationGroup';
+export type { UseGamepadNavigationGroupOptions } from './hooks/useGamepadNavigationGroup';
+export type { GamepadNavigationOptions } from './hooks/useGamepadNavigation';
+export type { TabsterDOMAttribute } from '@fluentui/react-tabster';
