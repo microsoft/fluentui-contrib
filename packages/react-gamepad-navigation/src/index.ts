9ce0cd9b4d11ff256a365ca27a556f1959202f48
Hector Jimenez <hectorjimenez@outlook.com>
2025-03-09 19:24:33 -0700

    removing 3rd party export

--- packages/react-gamepad-navigation/src/index.ts
+++ packages/react-gamepad-navigation/src/index.ts
@@ -1,4 +1,3 @@
 export { useGamepadNavigationGroup } from './hooks/useGamepadNavigationGroup';
 export type { UseGamepadNavigationGroupOptions } from './hooks/useGamepadNavigationGroup';
 export type { GamepadNavigationOptions } from './hooks/useGamepadNavigation';
-export type { TabsterDOMAttribute } from '@fluentui/react-tabster';
