bf1476dac409115e33cb7385822991c9084be7e6
Brandon Thomas <brandth@microsoft.com>
2025-04-09 15:37:44 -0700

    expanding tests to cover makeResetStyles

--- packages/token-analyzer/src/__tests__/e2e.test.ts
+++ packages/token-analyzer/src/__tests__/e2e.test.ts
@@ -44,24 +44,106 @@ describe('e2e test', () => {
     expect(styles).toHaveProperty('useRootBaseClassName');
   });
 
-  describe('validate makeResetStyles tokens', () => {
-    // Define token cases for makeResetStyles tests
-    const resetStyleTokenCases = [
-      ['backgroundColor', 'tokens.colorNeutralBackground1Hover'],
-      ['borderColor', 'tokens.colorNeutralStroke1Hover'],
-      ['color', 'tokens.colorNeutralForeground1Hover'],
-    ];
-    test.each(resetStyleTokenCases)(
+  /**
+   * Factory function that outputs a function we can test against. This defines the token test params
+   * that we will reuse across our tests. Note that this function must be called within the test.each() function
+   * to ensure that the test context is properly set up.
+   * @param tokenArray an array of tokens to pass into our factory
+   * @returns a function we will call within test.each()
+   */
+  const tokenTestFactory = (tokenArray: any) => {
+    return (propertyName: string, expectedToken: string) => {
+      const token = tokenArray.some(
+        (t: any) => t.property === propertyName && t.token === expectedToken
+      );
+      expect(token).toBeTruthy();
+    };
+  };
+
+  /**
+   * Reusable function to check tokens vs a known set
+   * @param tokenArray the token array to test. Must be in the form of a function due to the lifecycle of Jest
+   * @param testArray the known set of tokens are we looking for
+   */
+  const checkTokens = (tokenArray: () => any[], testArray: any[]) => {
+    test.each(testArray)(
       '%s token is properly configured',
       (propertyName, expectedToken) => {
-        const hoverMakeResetTokens =
-          styles.useRootBaseClassName.resetStyles.nested["':hover'"].tokens;
-        const token = hoverMakeResetTokens.find(
-          (t: any) => t.property === propertyName
-        );
-        expect(token).toBeDefined();
-        expect(token.token).toBe(expectedToken);
+        tokenTestFactory(tokenArray())(propertyName, expectedToken);
       }
+    );
+
+    // Check if the length of the token array matches the expected length
+    test(`token array length should be ${testArray.length}`, () => {
+      expect(tokenArray().length).toBe(testArray.length);
+    });
+  };
+
+  describe('validate makeResetStyles tokens', () => {
+    // Define token cases for hover makeResetStyles tests
+    checkTokens(
+      () => styles.useRootBaseClassName.resetStyles.nested["':hover'"].tokens,
+      [
+        ['backgroundColor', 'tokens.colorNeutralBackground1Hover'],
+        ['borderColor', 'tokens.colorNeutralStroke1Hover'],
+        ['color', 'tokens.colorNeutralForeground1Hover'],
+      ]
+    );
+
+    // Define token cases for active hover makeResetStyles tests
+    checkTokens(
+      () =>
+        styles.useRootBaseClassName.resetStyles.nested["':hover:active'"]
+          .tokens,
+      [
+        ['backgroundColor', 'tokens.colorNeutralBackground1Pressed'],
+        ['borderColor', 'tokens.colorNeutralStroke1Pressed'],
+        ['color', 'tokens.colorNeutralForeground1Pressed'],
+      ]
+    );
+
+    // base makeResetStyles tests
+    checkTokens(
+      () => styles.useRootBaseClassName.resetStyles.tokens,
+      [
+        ['backgroundColor', 'tokens.colorNeutralBackground1'],
+        ['color', 'tokens.colorNeutralForeground1'],
+        ['border', 'tokens.strokeWidthThin'],
+        ['border', 'tokens.colorNeutralStroke1'],
+        ['fontFamily', 'tokens.fontFamilyBase'],
+        ['padding', 'tokens.spacingHorizontalM'],
+        ['borderRadius', 'tokens.borderRadiusMedium'],
+        ['fontSize', 'tokens.fontSizeBase300'],
+        ['fontWeight', 'tokens.fontWeightSemibold'],
+        ['lineHeight', 'tokens.lineHeightBase300'],
+        ['transitionDuration', 'tokens.durationFaster'],
+        ['transitionTimingFunction', 'tokens.curveEasyEase'],
+      ]
+    );
+
+    // Token cases for makeResetStyles focus
+    checkTokens(
+      () => styles.useRootBaseClassName.resetStyles.nested[':focus'].tokens,
+      [
+        ['borderColor', 'tokens.colorStrokeFocus2'],
+        ['borderRadius', 'tokens.borderRadiusMedium'],
+        ['outline', 'tokens.strokeWidthThick'],
+        ['outline', 'tokens.strokeWidthThick'],
+        ['boxShadow', 'tokens.strokeWidthThin'],
+        ['boxShadow', 'tokens.colorStrokeFocus2'],
+      ]
+    );
+
+    // Token cases for makeResetStyles mozilla bug
+    checkTokens(
+      () =>
+        styles.useRootBaseClassName.resetStyles.nested[
+          "'@supports (-moz-appearance:button)'"
+        ].nested[':focus'].tokens,
+      [
+        ['boxShadow', 'tokens.colorStrokeFocus2'],
+        ['boxShadow', 'tokens.strokeWidthThin'],
+      ]
     );
   });
 
@@ -73,10 +155,10 @@ describe('e2e test', () => {
     test.each(makeStylesStyles)(
       '%s token is properly configured',
       (propertyName, expectedToken) => {
-        const rootOutline = styles.useRootStyles.outline.tokens;
-        const token = rootOutline.find((t: any) => t.property === propertyName);
-        expect(token).toBeDefined();
-        expect(token.token).toBe(expectedToken);
+        tokenTestFactory(styles.useRootStyles.outline.tokens)(
+          propertyName,
+          expectedToken
+        );
       }
     );
   });
