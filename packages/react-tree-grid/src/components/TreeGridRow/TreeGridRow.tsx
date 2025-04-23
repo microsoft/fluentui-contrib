34001bf295f69b376c33f6048b2182979d4e56a4
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2025-04-23 15:45:53 +0200

    feature(react-tree-grid): TreeGridRowTrigger component (#364)

--- packages/react-tree-grid/src/components/TreeGridRow/TreeGridRow.tsx
+++ packages/react-tree-grid/src/components/TreeGridRow/TreeGridRow.tsx
@@ -19,7 +19,6 @@ import {
   TabsterDOMAttribute,
   useMergedTabsterAttributes_unstable,
 } from '@fluentui/react-tabster';
-import { isHTMLElement } from '@fluentui/react-utilities';
 import { ArrowLeft, ArrowRight, Enter } from '@fluentui/keyboard-keys';
 import {
   TreeGridRowProvider,
@@ -70,10 +69,7 @@ export const TreeGridRow = React.forwardRef(
     const handleClick = useEventCallback(
       (event: React.MouseEvent<HTMLDivElement>) => {
         props.onClick?.(event);
-        if (!isHTMLElement(event.target)) {
-          return;
-        }
-        let element: HTMLElement | null = event.target;
+        let element = event.target as HTMLElement | SVGElement | null;
         while (element && element !== event.currentTarget) {
           if (element.tabIndex >= 0) return;
           element = element.parentElement;
@@ -103,14 +99,10 @@ export const TreeGridRow = React.forwardRef(
       { elementType: 'div' }
     );
     return (
-      <>
+      <TreeGridRowProvider value={context}>
         <Root />
-        {open && Subtree && (
-          <TreeGridRowProvider value={context}>
-            <Subtree />
-          </TreeGridRowProvider>
-        )}
-      </>
+        {open && Subtree && <Subtree />}
+      </TreeGridRowProvider>
     );
   }
 );
