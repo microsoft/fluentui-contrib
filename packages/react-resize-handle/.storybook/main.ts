eec1a36605b1f8a4be9b1294f677d036615aec48
Martin Hochel <hochelmartin@gmail.com>
2025-05-14 10:13:22 +0200

    chore(react-resize-handle): normalize SB configuration

--- packages/react-resize-handle/.storybook/main.ts
+++ packages/react-resize-handle/.storybook/main.ts
@@ -1,16 +1,11 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import rootConfig from '../../../.storybook/main';
+
 const config: StorybookConfig = {
+  ...rootConfig,
   stories: ['../stories/**/index.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@nx/react/plugins/storybook'],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
 };
 
 export default config;
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/packages/storybook/documents/custom-builder-configs
