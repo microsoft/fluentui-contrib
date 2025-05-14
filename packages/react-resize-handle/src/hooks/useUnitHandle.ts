6ecc62dfd449cea2d576330738822b54825fa832
Oleksandr Fediashov <olfedias@microsoft.com>
2025-05-14 12:03:48 +0200

    refactor to factories

--- packages/react-resize-handle/src/hooks/useUnitHandle.ts
+++ packages/react-resize-handle/src/hooks/useUnitHandle.ts
@@ -3,6 +3,11 @@ import * as React from 'react';
 
 import type { CSSUnit, CSSUnitName, GrowDirection } from '../types';
 
+type UnitHandleFactory = (params: {
+  getWindowDimensions: () => { width: number; height: number };
+  growDirection: GrowDirection;
+}) => UnitHandle;
+
 export type UnitHandle = {
   elementDimension: (element: HTMLElement | null) => number;
   fromPxToValue: (px: number) => number;
@@ -13,11 +18,91 @@ export type UnitHandle = {
 
 const DEFAULT_OFFSET_STEP = 20;
 
+function getElementDimensionInPx(
+  element: HTMLElement | null,
+  growDirection: GrowDirection
+): number {
+  if (element === null) {
+    return 0;
+  }
+
+  const key =
+    growDirection === 'end' || growDirection === 'start' ? 'width' : 'height';
+  const rect = element.getBoundingClientRect();
+
+  return rect[key];
+}
+
+const createPixelsHandle: UnitHandleFactory = ({ growDirection }) => {
+  const name: CSSUnitName = 'px';
+
+  function fromPxToValue(px: number) {
+    return px;
+  }
+
+  function roundValue(value: number): number {
+    return Math.round(value);
+  }
+
+  function getOffsetStep(): number {
+    return DEFAULT_OFFSET_STEP;
+  }
+
+  function elementDimension(element: HTMLElement | null): number {
+    return getElementDimensionInPx(element, growDirection);
+  }
+
+  return {
+    elementDimension,
+    fromPxToValue,
+    getOffsetStep,
+    roundValue,
+    name,
+  };
+};
+
+const createViewportHandle: UnitHandleFactory = ({
+  getWindowDimensions,
+  growDirection,
+}) => {
+  const name: CSSUnitName =
+    growDirection === 'end' || growDirection === 'start' ? 'vw' : 'vh';
+
+  function fromPxToValue(px: number) {
+    if (growDirection === 'end' || growDirection === 'start') {
+      return (px / getWindowDimensions().width) * 100;
+    }
+
+    return (px / getWindowDimensions().height) * 100;
+  }
+
+  function roundValue(value: number): number {
+    return Math.round((value + Number.EPSILON) * 1e5) / 1e5;
+  }
+
+  function getOffsetStep(): number {
+    return fromPxToValue(DEFAULT_OFFSET_STEP);
+  }
+
+  function elementDimension(element: HTMLElement | null): number {
+    return fromPxToValue(getElementDimensionInPx(element, growDirection));
+  }
+
+  return {
+    elementDimension,
+    fromPxToValue,
+    getOffsetStep,
+    roundValue,
+    name,
+  };
+};
+
 export function useUnitHandle(
   growDirection: GrowDirection,
   unit: CSSUnit
 ): UnitHandle {
   const { targetDocument } = useFluent();
+  const unitFactory = unit === 'px' ? createPixelsHandle : createViewportHandle;
 
   const windowDimensionsRef = React.useRef<{ width: number; height: number }>({
     width: 0,
@@ -54,62 +139,12 @@ export function useUnitHandle(
     }
   }, [targetDocument, unit]);
 
-  return React.useMemo(() => {
-    const name: CSSUnitName =
-      unit === 'px'
-        ? 'px'
-        : growDirection === 'end' || growDirection === 'start'
-        ? 'vw'
-        : 'vh';
-
-    function fromPxToValue(px: number) {
-      if (unit === 'px') {
-        return px;
-      }
-
-      if (unit === 'viewport') {
-        if (growDirection === 'end' || growDirection === 'start') {
-          return (px / windowDimensionsRef.current.width) * 100;
-        }
-
-        return (px / windowDimensionsRef.current.height) * 100;
-      }
-
-      return px;
-    }
-
-    function roundValue(value: number): number {
-      if (unit === 'px') {
-        return Math.round(value);
-      }
-
-      return Math.round((value + Number.EPSILON) * 1e5) / 1e5;
-    }
-
-    function getOffsetStep(): number {
-      return fromPxToValue(DEFAULT_OFFSET_STEP);
-    }
-
-    function elementDimension(element: HTMLElement | null): number {
-      if (element === null) {
-        return 0;
-      }
-
-      const key =
-        growDirection === 'end' || growDirection === 'start'
-          ? 'width'
-          : 'height';
-      const rect = element.getBoundingClientRect();
-
-      return fromPxToValue(rect[key]);
-    }
-
-    return {
-      elementDimension,
-      fromPxToValue,
-      getOffsetStep,
-      roundValue,
-      name,
-    };
-  }, [growDirection, unit]);
+  return React.useMemo(
+    () =>
+      unitFactory({
+        getWindowDimensions: () => windowDimensionsRef.current,
+        growDirection,
+      }),
+    [growDirection, unit]
+  );
 }
