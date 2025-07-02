a0e89eca4ea11fdd9550e9dd47f8adb774a7a396
Patrycja Fogelman <patrycja.fogelman@microsoft.com>
2025-07-02 14:41:51 +0200

    Fixing types in renderTooltip

--- packages/teams-components/src/components/ToggleButton/renderTooltip.tsx
+++ packages/teams-components/src/components/ToggleButton/renderTooltip.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Tooltip } from '@fluentui/react-components';
+import { StrictSlot } from '../../strictSlot';
 
-export const renderTooltip = (children: any, title: any) => {
+export const renderTooltip = (children: StrictSlot, title: StrictSlot) => {
   return (
     <Tooltip content={title} relationship="label">
       {children}
