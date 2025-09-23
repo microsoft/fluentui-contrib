a72a1fb619ec66cebaa10d7e71a7f2e9c25b95b3
Mitch-At-Work <mifraser@microsoft.com>
2025-09-23 10:45:08 -0700

    Finalize new vLength algo to not impact others

--- packages/react-virtualizer/src/hooks/useDynamicVirtualizerMeasure.ts
+++ packages/react-virtualizer/src/hooks/useDynamicVirtualizerMeasure.ts
@@ -36,6 +36,7 @@ export const useDynamicVirtualizerMeasure = <TElement extends HTMLElement>(
     virtualizerBufferSize: 0,
   });
 
+  const numItemsRef = React.useRef(numItems);
   const containerSizeRef = React.useRef<number>(0);
   const scrollPosition = React.useRef<number>(0);
   const { virtualizerLength, virtualizerBufferItems, virtualizerBufferSize } =
@@ -48,6 +49,7 @@ export const useDynamicVirtualizerMeasure = <TElement extends HTMLElement>(
       const hasReachedEnd =
         virtualizerContext.contextIndex + virtualizerLength >= numItems;
       if (!scrollRef?.current || hasReachedEnd) {
+        numItemsRef.current = numItems;
         // Error? ignore?
         return;
       }
@@ -117,17 +119,22 @@ export const useDynamicVirtualizerMeasure = <TElement extends HTMLElement>(
        */
       const newBufferSize = bufferSize ?? Math.max(defaultItemSize / 2, 1);
       const totalLength = length + newBufferItems * 2;
-      const newStartIndex = Math.max(
-        virtualizerContext.contextIndex + virtualizerLength - totalLength,
-        0
-      );
-      // We need to update the index to account for vLength changes
-      virtualizerContext.setContextIndex(newStartIndex);
+
+      if (numItemsRef.current !== numItems) {
+        const newStartIndex = Math.max(
+          virtualizerContext.contextIndex + virtualizerLength - totalLength,
+          0
+        );
+        // We need to update the index to account for vLength changes
+        virtualizerContext.setContextIndex(newStartIndex);
+        console.log('Calculate new index:', newStartIndex);
+      }
       setState({
         virtualizerLength: totalLength,
         virtualizerBufferSize: newBufferSize,
         virtualizerBufferItems: newBufferItems,
       });
+      numItemsRef.current = numItems;
     },
     [
       bufferItems,
