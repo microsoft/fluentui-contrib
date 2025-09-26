49e70f3c34971bfff516f4553cbbd5d0835d6c8d
Mitch-At-Work <mifraser@microsoft.com>
2025-09-26 09:32:39 -0700

    Ignore transitions for now, could be done smoother but still works (should animate internal components, or only animate items in-view)

--- packages/react-virtualizer/stories/VirtualizerScrollViewDynamic/ComplexDynamicList.stories.tsx
+++ packages/react-virtualizer/stories/VirtualizerScrollViewDynamic/ComplexDynamicList.stories.tsx
@@ -157,7 +157,7 @@ const LazyLoadingComponent = React.forwardRef(
         style={{
           boxSizing: 'border-box', // If you want border/padding etc. on the outside div, use border-box
           minHeight: totalHeight, // This will jump from ~60px to ~2000px+
-          transition: 'min-height 0.2s ease', // Transitions work, but they cause scrollAnchor to be a little jumpy
+          // transition: 'min-height 0.2s ease', // Transitions work, but they cause scrollAnchor to be a little jumpy
           border: `2px solid ${massiveContentLoaded ? '#ff4444' : '#ddd'}`, // Visual indicator
         }}
       >
