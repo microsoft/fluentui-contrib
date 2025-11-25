1e40d615b218a7ca4b8a6c29b839a85dfb456c30
David Zukowski <dzukowski@microsoft.com>
2025-11-24 16:53:49 -0800

    Update react-cap-theme structure to follow new guidelines

--- packages/react-cap-theme/src/components/Input/Input.styles.ts
+++ packages/react-cap-theme/src/components/Input/Input.styles.ts
@@ -4,13 +4,12 @@ import {
   type InputState,
 } from '@fluentui/react-components';
 
-const useCAPInputStyles = makeStyles({
+export const useInputStyles = makeStyles({
   root: {},
 });
 
-export function useCAPInputStylesHook(_state: unknown): InputState {
-  const state = _state as InputState;
-  const styles = useCAPInputStyles();
+export function useInputStylesHook(state: InputState): InputState {
+  const styles = useInputStyles();
 
   state.root.className = mergeClasses(state.root.className, styles.root);
 
