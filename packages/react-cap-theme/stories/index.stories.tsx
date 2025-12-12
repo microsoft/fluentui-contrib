b44fbe74172aeff3aca5dab3dbd5e35cb860bbeb
Yi Zhang <yizhang9@microsoft.com>
2025-12-12 08:43:22 -0800

    re-order imports

--- packages/react-cap-theme/stories/index.stories.tsx
+++ packages/react-cap-theme/stories/index.stories.tsx
@@ -4,12 +4,12 @@ import { Meta } from '@storybook/react';
 export { CAPBadgeStory as Badge } from './components/Badge.stories';
 export { CAPButtonWithCtrlsStory as ButtonWithControls } from './components/Button.stories';
 export { CAPCardStory as Card } from './components/Card.stories';
+export { CAPDialogStory as Dialog } from './components/Dialog/WithTable';
+export { CAPDialogWithCtrlsStory as DialogWithControls } from './components/Dialog/WithControls.stories';
 export { CAPDrawerWithCtrlsStory as DrawerWithControls } from './components/Drawer.stories';
 export { CAPInputStory as Input } from './components/Input.stories';
 export { CAPMenuStory as Menu } from './components/Menu.stories';
 export { CAPTooltipStory as Tooltip } from './components/Tooltip.stories';
-export { CAPDialogStory as Dialog } from './components/Dialog/WithTable';
-export { CAPDialogWithCtrlsStory as DialogWithControls } from './components/Dialog/WithControls.stories';
 
 const VisualRefreshStory = () => <div />;
 
