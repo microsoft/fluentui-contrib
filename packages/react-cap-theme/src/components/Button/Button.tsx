c1b698715e26465469dfa7b9608bd149ee215537
David Zukowski <dzukowski@microsoft.com>
2026-03-09 21:06:56 -0700

    react-cap-theme: publish 1074d9c

--- packages/react-cap-theme/src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import type { ForwardRefComponent } from '@fluentui/react-utilities';
-import { Button as FluentButton } from '@fluentui/react-components';
-import type { ButtonProps } from './Button.types';
-
-export const Button = FluentButton as ForwardRefComponent<ButtonProps>;
-
-Button.displayName = 'Button';
--- packages/react-cap-theme/src/index.ts
+++ packages/react-cap-theme/src/index.ts
@@ -1,11 +1,38 @@
-export { CAPThemeProvider } from './theme/CAPThemeProvider';
+import { FluentProviderProps } from "@fluentui/react-components";
+import {
+	useButtonStyles,
+	useMenuButtonStyles,
+	useSplitButtonStyles,
+	useToggleButtonStyles,
+	type ButtonState,
+	type MenuButtonState,
+	type SplitButtonState,
+	type ToggleButtonState,
+} from "./components/react-button";
+
+export { getIntrinsicElementProps, slot } from "@fluentui/react-utilities";
 export {
-  CAP_THEME_ONE_DRIVE,
-  CAP_THEME_SHAREPOINT,
-  CAP_THEME_TEAMS,
-} from './theme/CAPTheme';
-export { Button } from './components/Button/Button';
-export type {
-  ButtonProps,
-  ButtonState,
-} from './components/Button/Button.types';
+	motionTokens,
+	makeStyles,
+	mergeClasses,
+} from "@fluentui/react-components";
+
+export * from "./components/react-button";
+export * from "./components/tokens";
+
+export const CAP_STYLE_HOOKS: NonNullable<
+	FluentProviderProps["customStyleHooks_unstable"]
+> = {
+	useButtonStyles_unstable: (state) => {
+		return useButtonStyles(state as ButtonState);
+	},
+	useMenuButtonStyles_unstable: (state) => {
+		return useMenuButtonStyles(state as MenuButtonState);
+	},
+	useSplitButtonStyles_unstable: (state) => {
+		return useSplitButtonStyles(state as SplitButtonState);
+	},
+	useToggleButtonStyles_unstable: (state) => {
+		return useToggleButtonStyles(state as ToggleButtonState);
+	},
+};
