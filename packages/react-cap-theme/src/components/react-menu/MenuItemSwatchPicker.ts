2909bb051313195e3308a6dabb9a17e100bec062
Yi Zhang <yizhang9@microsoft.com>
2026-04-01 11:30:39 -0700

    remove menu component

--- packages/react-cap-theme/src/components/react-menu/MenuItemSwatchPicker.ts
+++
@@ -1,7 +0,0 @@
-export { MenuItemSwatchPicker } from './components/MenuItemSwatchPicker/MenuItemSwatchPicker';
-export type { MenuItemSwatchPickerProps } from './components/MenuItemSwatchPicker/MenuItemSwatchPicker.types';
-export { useMenuItemSwatchPicker } from './components/MenuItemSwatchPicker/useMenuItemSwatchPicker';
-export {
-  menuItemSwatchPickerClassNames,
-  useMenuItemSwatchPickerStyles,
-} from './components/MenuItemSwatchPicker/useMenuItemSwatchPickerStyles.styles';
--- packages/react-cap-theme/src/components/react-menu/components/MenuItemSwatchPicker/MenuItemSwatchPicker.types.ts
+++
@@ -1,5 +0,0 @@
-import type { MenuItemSelectableProps } from '@fluentui/react-menu';
-import type { SwatchPickerProps } from '@fluentui/react-swatch-picker';
-
-export type MenuItemSwatchPickerProps = Pick<MenuItemSelectableProps, 'name'> &
-  SwatchPickerProps;
