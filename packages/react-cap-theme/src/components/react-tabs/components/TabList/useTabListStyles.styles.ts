ad57c12c09557c2a1811382606e4ccf0113da674
Enrico Gianoglio <enry8giano@gmail.com>
2026-05-08 18:25:26 +0200

    feat(react-cap-theme): add `react-tabs` components (#647)
    Co-authored-by: EnricoGianoglio <egianoglio@microsoft.com>

---
+++ packages/react-cap-theme/src/components/react-tabs/components/TabList/useTabListStyles.styles.ts
@@ -0,0 +1,30 @@
+import { makeStyles, mergeClasses } from '@griffel/react';
+import { tokens } from '@fluentui/tokens';
+import { getSlotClassNameProp_unstable } from '@fluentui/react-utilities';
+import type { TabListState } from '@fluentui/react-tabs';
+
+const useStyles = makeStyles({
+  horizontal: { gap: tokens.spacingHorizontalS },
+  vertical: { alignItems: 'start', gap: tokens.spacingVerticalSNudge },
+  verticalLarge: { gap: tokens.spacingVerticalS },
+});
+
+/**
+ * Applies styles to the TabList component.
+ * @param state - The TabList component state object.
+ * @returns The updated state object with applied styles.
+ * @alpha
+ */
+export const useTabListStyles = (state: TabListState): TabListState => {
+  const styles = useStyles();
+  const { size, vertical } = state;
+
+  state.root.className = mergeClasses(
+    state.root.className,
+    vertical ? styles.vertical : styles.horizontal,
+    vertical && size === 'large' && styles.verticalLarge,
+    getSlotClassNameProp_unstable(state.root)
+  );
+
+  return state;
+};
