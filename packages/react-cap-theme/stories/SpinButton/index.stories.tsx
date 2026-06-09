eb3f9ffae06f781863788ba6e4044e4d66e9417d
Alexander Katrukhin <Oleksandr.Katrukhin@microsoft.com>
2026-06-08 22:13:30 -0400

    docs(react-cap-theme): add SpinButton storybook stories
    Default, Appearance, Size, and Disabled stories adapted from the Fluent
    react-spinbutton examples. Rendered under CAP_STYLE_HOOKS via the storybook
    decorator to visually verify the CAP outline overrides.

---
+++ packages/react-cap-theme/stories/SpinButton/index.stories.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import type { Meta } from '@storybook/react';
+import { SpinButton } from '@fluentui/react-components';
+
+const meta = {
+  title: 'Packages/react-cap-theme/SpinButton',
+  component: SpinButton,
+} satisfies Meta<typeof SpinButton>;
+
+export default meta;
+
+export { Default } from './Default.stories';
+export { Appearance } from './Appearance.stories';
+export { Size } from './Size.stories';
+export { Disabled } from './Disabled.stories';
